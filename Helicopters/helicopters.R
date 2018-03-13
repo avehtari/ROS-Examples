b500c3d4c1fe895538e485bb949c90591d6b0694
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-03-13 18:10:55 +0200

    updated Helicopters

--- Helicopters/helicopters.R
+++ Helicopters/helicopters.R
@@ -1,2 +1,20 @@
-setwd("~/AndrewFiles/books/regression.and.other.stories/Examples/Helicopters")
-helicopters <- read.table("helicopters.txt", header=TRUE)
+#' ---
+#' title: "Regression and Other Stories: Helicopters"
+#' author: "Andrew Gelman, Aki Vehtari"
+#' date: "`r format(Sys.Date())`"
+#' ---
+
+#' Example data file for helicopter flying time exercise
+#' 
+#' -------------
+#' 
+
+#' **Load libraries**
+#+ setup, message=FALSE, error=FALSE, warning=FALSE
+library("here")
+
+#' **Load data**
+helicopters <- read.table(here("Helicopters/data","helicopters.txt"), header=TRUE)
+
+#' **Display the example data**
+print(helicopters)
