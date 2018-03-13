a034df64d827cced7b5f9683d96612a6e4c4b986
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-03-13 18:07:41 +0200

    updated DifferentSoftware

--- DifferentSoftware/linear.R
+++ DifferentSoftware/linear.R
@@ -1,15 +1,24 @@
-setwd("~/AndrewFiles/books/regression.and.other.stories/Examples/DifferentSoftware")
+#' ---
+#' title: "Regression and Other Stories: Linear Regression"
+#' author: "Andrew Gelman, Aki Vehtari"
+#' date: "`r format(Sys.Date())`"
+#' ---
+#' 
 
-## Linear regression using different software options
+#' Linear regression using different software options
+#' 
+#' -------------
+#' 
 
-## Load everything in
+#' **Load libraries**
+#+ setup, message=FALSE, error=FALSE, warning=FALSE
 library("arm")
 library("rstan")
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 library("rstanarm")
 
-## Create fake data
+#' **Create fake data**
 N <- 100
 b <- 1:3
 x1 <- rnorm(N)
@@ -19,29 +28,28 @@ sigma <- 2
 y <- X %*% b + rnorm(N, 0, sigma)
 dat <- data.frame(y, x1, x2)
 
-## Fit and display using lm, listing predictors one at a time
+#' **Fit and display using lm, listing predictors one at a time**
 fit1 <- lm(y ~ x1 + x2, data = dat)
 display(fit1)
 
-## Extract estimates and uncertainties
+#' **Extract estimates and uncertainties**
 b_hat <- coef(fit1)
 b_se <- se.coef(fit1)
 print(cbind(b_hat, b_se))
 
-## Fit and display using lm, using matrix of predictors
+#' **Fit and display using lm, using matrix of predictors**
 fit2 <- lm(y ~ X)
 display(fit2)
 
-## FIt and display using stan_glm
+#' **Fit and display using stan_glm**
 fit3 <- stan_glm(y ~ x1 + x2, data = dat)
 print(fit3)
 print(fit3, digits=2)
-## Run again just to see some simulation variability
+#' **Run again just to see some simulation variability**
 fit3 <- stan_glm(y ~ x1 + x2, data = dat)
 print(fit3, digits=2)
 
-## Extract estimates and uncertainties
+#' **Extract estimates and uncertainties**
 b_hat <- coef(fit3)
 b_se <- se(fit3)
 print(cbind(b_hat, b_se))
-
