8b115e1f905a0fb1bcb52fd099729a0df7f75cf0
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-03-13 19:50:58 +0100

    updated Mile

--- Mile/mile.R
+++ Mile/mile.R
@@ -1,23 +1,38 @@
-setwd("~/AndrewFiles/books/regression.and.other.stories/Examples/Mile")
+#' ---
+#' title: "Regression and Other Stories: Mile"
+#' author: "Andrew Gelman, Aki Vehtari"
+#' date: "`r format(Sys.Date())`"
+#' ---
+
+#' Trend of record times in the mile run
+#' 
+#' -------------
+#' 
+
+#' **Load libraries**
+#+ setup, message=FALSE, error=FALSE, warning=FALSE
+library("here")
 library("arm")
 library("rstanarm")
 options(mc.cores = parallel::detectCores())
 
-mile <- read.table("mile2.txt", header=TRUE)
-
+#' **Load data**
+mile <- read.table(here("Mile/data","mile2.txt"), header=TRUE)
 year <- mile$yr + mile$month/12
 seconds <- mile$min*60 + mile$sec
 
+#' **Linear model*
 fit <- lm(seconds ~ year)
 display(fit, 3)
 
-## Predictions for 1900 and 2000
-
+#' **Predictions for 1900 and 2000**
 print(1007 -.393*c(1900,2000))  # Approx
 print(coef(fit)[1] + coef(fit)[2]*c(1900,2000)) # Exact
 
-
-pdf ("aplusbx1a.pdf", height=3.5, width=5)
+#' **Example of increasing trend**
+#+ eval=FALSE, include=FALSE
+pdf(here("Mile/figs","aplusbx1a.pdf"), height=3.5, width=5)
+#+
 a <- 0.15
 b <- 0.4
 par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
@@ -26,9 +41,13 @@ plot (c(0,2.2), c(0,a+2.2*b), pch=20, cex=.5, main="y = a + bx",
 axis (1, c(0,1,2))
 axis (2, c(a,a+b,a+2*b), c("a","a+b","a+2b"))
 abline (a, b)
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-pdf ("aplusbx1b.pdf", height=3.5, width=5)
+#' **Example of decreasing trend**
+#+ eval=FALSE, include=FALSE
+pdf(here("Mile/figs","aplusbx1b.pdf"), height=3.5, width=5)
+#+
 a <- 0.95
 b <- -0.4
 par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
@@ -37,22 +56,35 @@ plot (c(0,2.2), c(0,a+.2), pch=20, cex=.5, main="y = a + bx (with b < 0)",
 axis (1, c(0,1,2))
 axis (2, c(a,a+b,a+2*b), c("a","a+b","a+2b"))
 abline (a, b)
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-pdf ("aplusbx2a.pdf", height=3.5, width=5)
+#' **Approximate trend from the fit in range [0,2.1]**
+#+ eval=FALSE, include=FALSE
+pdf(here("Mile/figs","aplusbx2a.pdf"), height=3.5, width=5)
+#+
 par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
 curve (1007 - 0.393*x, from=0, to=2.1, xlab="x", ylab="y", bty="l",
   main="y = 1007 - 0.393x")
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-pdf ("aplusbx2b.pdf", height=3.5, width=5)
+#' **Approximate trend from the fit in range [0,100]**
+#+ eval=FALSE, include=FALSE
+pdf(here("Mile/figs","aplusbx2b.pdf"), height=3.5, width=5)
+#+
 par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
 curve (1007 - 0.393*x, from=0, to=100, xlab="x", ylab="y", bty="l",
   main="y = 1007 - 0.393x")
+#+ eval=FALSE, include=FALSE
 dev.off()     
 
-pdf ("aplusbx3.pdf", height=3.5, width=5)
+#' **Approximate trend of record times in the mile run from 1900 to 2000**
+#+ eval=FALSE, include=FALSE
+pdf(here("Mile/figs","aplusbx3.pdf"), height=3.5, width=5)
+#+
 par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
 curve (1007 - 0.393*x, from=1900, to=2000, xlab="Year", ylab="Time (seconds)", bty="l",
   main="Approx. trend of record times in the mile run", ylim=c(210, 270))
+#+ eval=FALSE, include=FALSE
 dev.off()
