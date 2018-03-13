ba819d52f63ce287854957fee88bd9fde2e327f9
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-03-13 18:54:04 +0100

    updated Golf

--- Golf/golf.R
+++ Golf/golf.R
@@ -1,13 +1,24 @@
-setwd("~/AndrewFiles/books/regression.and.other.stories/Examples/Golf")
+#' ---
+#' title: "Regression and Other Stories: Golf"
+#' author: "Andrew Gelman, Aki Vehtari"
+#' date: "`r format(Sys.Date())`"
+#' ---
 
+#' Gold putting accuracy: Fitting a nonlinear model using Stan
+#' 
+#' -------------
+#' 
+
+#' **Load libraries**
+#+ setup, message=FALSE, error=FALSE, warning=FALSE
+library("here")
 library("arm")
 library("rstan")
-library("rstanarm")
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-## Set up the data
-golf <- read.table("golf.txt", header=TRUE, skip=2)
+#' **Set up the data**
+golf <- read.table(here("Golf/data","golf.txt"), header=TRUE, skip=2)
 x <- golf$x
 y <- golf$y
 n <- golf$n
@@ -16,40 +27,56 @@ r <- (1.68/2)/12
 R <- (4.25/2)/12
 se <- sqrt((y/n)*(1-y/n)/n)
 
-## Fit the model
-fit1 <- stan("golf1.stan")
-print(fit1)
-
-## Post-processing
-sims1 <- extract(fit1)
-sigma_hat <- median(sims1$sigma)
-
-## Naive logistic regression
-fit2 <- stan("golf_logistic.stan")
-print(fit2)
-
-## Post-processing
-sims2 <- extract(fit2)
-a_hat <- median(sims2$a)
-b_hat <- median(sims2$b)
-
-## Plots
-pdf(here("Golf/figs","golf0.pdf") height=5, width=7)
+#' **Plot data**
+#+ eval=FALSE, include=FALSE
+pdf(here("Golf/figs","golf0.pdf"), height=5, width=7)
+#+
 par(mar=c(3,3,2,1), mgp=c(1.7,.5,0), tck=-.02)
 plot(x, y/n, xlim=c(0, 1.1*max(x)), ylim=c(0, 1.02), xaxs="i", yaxs="i", pch=20, bty="l", xlab="Distance from hole (feet)", ylab="Probability of success", main="Data on putts in pro golf")
 segments(x, y/n + se, x, y/n-se, lwd=.5)
 text(x + .4, y/n + se + .02, paste(y, "/", n,sep=""), cex=.6, col="gray40") 
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-pdf(here("Golf/figs","golf1.pdf") height=5, width=7)
+#' **Fit the geometry based nonlinear model**
+writeLines(readLines(here("Golf","golf1.stan")))
+#+ results='hide'
+fit1 <- stan(here("Golf","golf1.stan"))
+#+
+print(fit1)
+
+#' Post-processing
+sims1 <- extract(fit1)
+sigma_hat <- median(sims1$sigma)
+
+#' **Fit naive logistic regression**
+writeLines(readLines(here("Golf","golf_logistic.stan")))
+#+ results='hide'
+fit2 <- stan(here("Golf","golf_logistic.stan"))
+#+
+print(fit2)
+
+#' Post-processing
+sims2 <- extract(fit2)
+a_hat <- median(sims2$a)
+b_hat <- median(sims2$b)
+
+#' **Plot logistic regression result**
+#+ eval=FALSE, include=FALSE
+pdf(here("Golf/figs","golf1.pdf"), height=5, width=7)
+#+
 par(mar=c(3,3,2,1), mgp=c(1.7,.5,0), tck=-.02)
 plot(x, y/n, xlim=c(0, 1.1*max(x)), ylim=c(0, 1.02), xaxs="i", yaxs="i", pch=20, bty="l", xlab="Distance from hole (feet)", ylab="Probability of success", main="Fitted logistic regression")
 segments(x, y/n + se, x, y/n-se, lwd=.5)
 curve(invlogit(a_hat + b_hat*x), from=0, to=1.1*max(x), add=TRUE)
 text(10.6, .57, paste("Logistic regression,\n    a = ", fround(a_hat, 2), ", b = ", fround(b_hat, 2), sep=""))
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-pdf(here("Golf/figs","golf2.pdf") height=5, width=7)
+#' **Plot geometry based model result**
+#+ eval=FALSE, include=FALSE
+pdf(here("Golf/figs","golf2.pdf"), height=5, width=7)
+#+
 par(mar=c(3,3,2,1), mgp=c(1.7,.5,0), tck=-.02)
 plot(x, y/n, xlim=c(0, 1.1*max(x)), ylim=c(0, 1.02), xaxs="i", yaxs="i", pch=20, bty="l", xlab="Distance from hole (feet)", ylab="Probability of success", main="Custom nonlinear model fit in Stan")
 segments(x, y/n + se, x, y/n-se, lwd=.5)
@@ -57,10 +84,13 @@ x_grid <- seq(R-r, 1.1*max(x), .01)
 p_grid <- 2*pnorm(asin((R-r)/x_grid) / sigma_hat) - 1
 lines(c(0, R-r, x_grid), c(1, 1, p_grid))
 text(18.5, .26, paste("Geometry-based model,\n sigma = ", fround(sigma_hat*180/pi, 1), " degrees", sep=""))
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-
-pdf(here("Golf/figs","golf2a.pdf") height=5, width=7)
+#' **Plot geometry based model posterior draws of sigma**
+#+ eval=FALSE, include=FALSE
+pdf(here("Golf/figs","golf2a.pdf"), height=5, width=7)
+#+
 par(mar=c(3,3,2,1), mgp=c(1.7,.5,0), tck=-.02)
 plot(x, y/n, xlim=c(0, 1.1*max(x)), ylim=c(0, 1.02), xaxs="i", yaxs="i", pch=20, bty="l", xlab="Distance from hole (feet)", ylab="Probability of success", main="Custom nonlinear model fit in Stan")
 segments(x, y/n + se, x, y/n-se, lwd=.5)
@@ -71,9 +101,13 @@ for (s in sample(n_sims, 20)){
   lines(c(0, R-r, x_grid), c(1, 1, p_grid), lwd=0.5)
 }
 text(18.5, .26, "Geometry-based model,\n post draws of sigma")
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-pdf(here("Golf/figs","golf3.pdf") height=5, width=7)
+#' **Plot two models in same figure**
+#+ eval=FALSE, include=FALSE
+pdf(here("Golf/figs","golf3.pdf"), height=5, width=7)
+#+
 par(mar=c(3,3,2,1), mgp=c(1.7,.5,0), tck=-.02)
 plot(x, y/n, xlim=c(0, 1.1*max(x)), ylim=c(0, 1.02), xaxs="i", yaxs="i", pch=20, bty="l", xlab="Distance from hole (feet)", ylab="Probability of success", main="Two models fit to the golf putting data")
 segments(x, y/n + se, x, y/n-se, lwd=.5)
@@ -83,4 +117,5 @@ p_grid <- 2*pnorm(asin((R-r)/x_grid) / sigma_hat) - 1
 lines(c(0, R-r, x_grid), c(1, 1, p_grid))
 text(10.3, .58, "Logistic regression")
 text(18.5, .24, "Geometry-based model")
+#+ eval=FALSE, include=FALSE
 dev.off()
