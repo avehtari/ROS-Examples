2194f258f0aefb4d7f362f326d4542d93b3faaee
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-03-30 18:12:51 +0300

    index

---
+++ index.R
@@ -0,0 +1,46 @@
+#' ---
+#' title: "Regression and Other Stories: index"
+#' author: "Andrew Gelman, Jennifer Hill, Aki Vehtari"
+#' date: "`r format(Sys.Date())`"
+#' ---
+
+#' Index for the code and data for the book Regression and Other Stories
+#' 
+#' -------------
+#' 
+
+#' ### Code and data by chapters
+#' 
+
+#' ### Introduction
+#' - [ElectionsEconomy](ElectionsEconomy/)
+#' - [Helicopters](Helicopters/)
+#' 
+#' ### Data and measurement
+#'  - healthdata.txt missing
+#' 
+#' ### Some basic methods in mathematics and probability
+#'  - [Mile](Mile/)
+#' 
+#' ### Sampling distributions, generative models, and statistical inference
+#'   - [Girls](Girls/)
+#' 
+#' ### Simulation
+#'   - [ProbabilitySimulation](ProbabilitySimulation/)
+#'   - [Earnings](Earnings/)
+#' 
+#' ### Background on regression modeling
+#'   - [Earnings](Earnings/)
+#'   - [PearsonLee](PearsonLee/)
+#'   - [FakeMidtermFinal](FakeMidtermFinal/)
+#' 
+#' ### Linear regression with a single predictor
+#'   - [Simplest](Simplest/)
+#'   - [ElectionsEconomy](ElectionsEconomy/)
+#' 
+#' ### Prediction and Bayesian inference
+#'   - [ElectionsEconomy](ElectionsEconomy/)
+#'   - [Vitals](Vitals/)
+#'   - [SexRatio](SexRatio/)
+#'   - [Earnings](Earnings/)
+#' 
