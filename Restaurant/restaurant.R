541a4ffa29f69953d810405dc3b7629a69774afa
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-04-25 10:21:06 -0400

    updated Restaurant

--- Restaurant/restaurant.R
+++ Restaurant/restaurant.R
@@ -1,10 +1,29 @@
-setwd("~/AndrewFiles/books/regression.and.other.stories/Examples/Restaurant")
+#' ---
+#' title: "Regression and Other Stories: Imputation"
+#' author: "Andrew Gelman, Jennifer Hill, Aki Vehtari"
+#' date: "`r format(Sys.Date())`"
+#' ---
 
+#' Regression-based imputation for the Social Indicators Survey
+#' 
+#' -------------
+#' 
+
+#' **Load libraries**
+#+ setup, message=FALSE, error=FALSE, warning=FALSE
+library("rprojroot")
+root<-has_dirname("RAOS-Examples")$make_fix_file()
 library("rstan")
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
+#' **Show Stan code**
+writeLines(readLines(root("Restaurant","restaurant.stan")))
+
+#' **Compile Stan code**
+#+ results='hide'
 resto <- stan_model("restaurant.stan")
+
+#' **Optimize**
 fit <- optimizing(resto)
 print(fit)
-
