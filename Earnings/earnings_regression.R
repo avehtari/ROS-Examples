7f8bd4b719dd4bfb2688ebd09cb4217635038602
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-05-28 21:24:21 +0300

    earnings_regression lm -> stan_glm

--- Earnings/earnings_regression.R
+++ Earnings/earnings_regression.R
@@ -13,7 +13,6 @@
 #+ setup, message=FALSE, error=FALSE, warning=FALSE
 library("rprojroot")
 root<-has_dirname("RAOS-Examples")$make_fix_file()
-library("arm")
 library("rstanarm")
 options(mc.cores = parallel::detectCores())
 library("ggplot2")
@@ -23,54 +22,61 @@ theme_set(bayesplot::theme_default(base_family = "sans"))
 #' **Load data**
 earnings_all <- read.csv(root("Earnings/data","earnings.csv"))
 earnings_all$positive <- earnings_all$earn > 0
+# scale earnings to thousands of dollars
+earnings_all$earnk <- earnings_all$earn/1000 
 # only non-zero earnings
 earnings <- earnings_all[earnings_all$positive, ]
 n <- nrow(earnings)
 height_jitter_add <- runif(n, -.2, .2)
 
-#' ### Classical regression
+#' ### Normal linear regression
 
-#' **Model on original scale**
-lm_0 <- lm(earn ~ height, data=earnings)
-display(lm_0)
+#' **Model on 1k dollars scale**
+#+ results='hide'
+fit_1 <- stan_glm(earnk ~ height, data=earnings)
+#+
+print(fit_1)
+#' for plotting scale back to dollar scale
+coef1 <- coef(fit_1)*1000
 
+#' **Plot linear model**
 #+ eval=FALSE, include=FALSE
-# figure for the book, don't display in the report
 pdf(root("Earnings/figs","heights1a.pdf"), height=8.5, width=11)
-# plot linear model
+#+
 par(mar=c(6,6,4,2)+.1)
 plot(earnings$height + height_jitter_add, earnings$earn, xlab="height", ylab="earnings",
      cex=.8, cex.lab=3, pch=20, cex.axis=3, yaxt="n", mgp=c(4,1.5,0),
      col="gray10",  cex.main=3, main="Fitted linear model")
-abline(coef(lm_0), lwd=2)
+abline(coef1, lwd=2)
 abline(0,0,col="gray")
 axis(2, c(0,100000,200000), c("0","100000","200000"), mgp=c(4,1.1,0),cex.axis=3)
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-#' plot linear model, ggplot version
+#' **Plot linear model, ggplot version**
 gg_earnings <- ggplot(earnings, aes(x = height + height_jitter_add, y = earn)) +
   geom_point(alpha = 0.75) +
   geom_hline(yintercept = 0, color = "darkgray") +
-  geom_abline(intercept = coef(lm_0)[1], slope = coef(lm_0)[2], size = 1) +
+  geom_abline(intercept = coef1[1], slope = coef1[2], size = 1) +
   labs(x = "height", y = "earnings",
        title = "Fitted linear model")
 gg_earnings
 
-
+#' **Plot extrapolation**
 #+ eval=FALSE, include=FALSE
-# figure for the book, don't display in the report
-# plot extrapolation
 pdf(root("Earnings/figs","heights1b.pdf"), height=8.5, width=11)
+#+
 par(mar=c(6,6,4,2)+.1)
 plot(xlim=c(0,max(earnings$height)), ylim=c(-70000,200000), earnings$height + height_jitter_add, earnings$earn, xlab="height", ylab="earnings", cex=.8, cex.lab=3, pch=20, cex.axis=3, yaxt="n", mgp=c(4,1.5,0), col="gray10", cex.main=3, main="Extrapolation")
-abline(coef(lm_0), lwd=2)
+abline(coef1, lwd=2)
 abline(0,0,col="gray")
-intercept <- coef(lm_0)[1]
+intercept <- coef1[1]
 axis(2, c(intercept,0,100000,200000), c(round(intercept,-2),"0","100000","200000"), mgp=c(4,1.1,0),cex.axis=3)
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-#' Plot extrapolation, ggplot version, modifying the gg_earnings object
-#' we already created
+#' **Plot extrapolation, ggplot version, modifying the gg_earnings object
+#' we already created**
 gg_earnings +
   ylim(-70000, 200000) +
   xlim(0, 80) +
@@ -78,83 +84,93 @@ gg_earnings +
 
 
 #' **Include male/female**
-lm_1 <- lm(earn ~ height + male, data=earnings)
-display(lm_1)
-coef1 <- coef(lm_1)
+#+ results='hide'
+fit_2 <- stan_glm(earnk ~ height + male, data=earnings)
+#+
+print(fit_2)
+#' for plotting scale back to dollar scale
+coef2 <- coef(fit_2)*1000
 
+#' **Plot linear model with male/female**
 #+ eval=FALSE, include=FALSE
-# figure for the book, don't display in the report
 pdf(root("Earnings/figs","heights2.pdf"), height=8.5, width=11)
+#+
 par(mar=c(6,6,5,2)+.1)
-plot(range(earnings$height), range(predict(lm_1)), type="n", xlab="height", ylab="predicted earnings", cex=.8, cex.lab=3, pch=20, cex.axis=3, mgp=c(4,1.5,0), yaxt="n", col="gray10",
+plot(range(earnings$height), range(predict(fit_2)*1000), type="n", xlab="height", ylab="predicted earnings", cex=.8, cex.lab=3, pch=20, cex.axis=3, mgp=c(4,1.5,0), yaxt="n", col="gray10",
       cex.main=3, main="Fitted regression, displayed as\nseparate lines for men and women", bty="l")
 axis(2, c(20000,30000), cex.axis=3)
-abline(coef1[1], coef1[2], col="red", lwd=2)
-text(68, coef1[1] + coef1[2]*65, "women:\ny = -11 000 + 450x", cex=3, adj=0, col="red")
-abline(coef1[1]+coef1[3], coef1[2], col="blue", lwd=2)
-text(68, coef1[1]+coef1[3] + coef1[2]*65, "men:\ny = -2 000 + 450x", cex=3, adj=0, col="blue")
+abline(coef2[1], coef2[2], col="red", lwd=2)
+text(68, coef2[1] + coef2[2]*65, "women:\ny = -11 000 + 450x", cex=3, adj=0, col="red")
+abline(coef2[1]+coef2[3], coef2[2], col="blue", lwd=2)
+text(68, coef2[1]+coef2[3] + coef2[2]*65, "men:\ny = -2 000 + 450x", cex=3, adj=0, col="blue")
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-#' include male/female, ggplot version
+#' **Include male/female, ggplot version**
 ggplot(earnings, aes(height, earn)) +
   geom_blank() +
   geom_abline(
-    intercept = c(coef1[1], coef1[1] + coef1[3]),
-    slope = coef1[2],
+    intercept = c(coef2[1], coef2[1] + coef2[3]),
+    slope = coef2[2],
     color = c("red", "blue")
   ) +
   coord_cartesian(
-    ylim = range(predict(lm_1)),
+    ylim = range(predict(fit_2)*1000),
     xlim = range(earnings$height)
   ) +
   annotate(
     "text",
     x = c(68, 68),
-    y = c(coef1[1] + coef1[2] * 65, coef1[1] + coef1[3] + coef1[2] * 65),
+    y = c(coef2[1] + coef2[2] * 65, coef2[1] + coef2[3] + coef2[2] * 65),
     label = c("women:\ny = -11 000 + 450x", "men:\ny = -2 000 + 450x"),
     color = c("red", "blue"),
     size = 5, hjust = 0
   ) +
   labs(
     x = "height",
-      y = "predicted earnings",
+    y = "predicted earnings",
     title = "Fitted regression, displayed as\nseparate lines for men and women"
   )
 
 
 #' **Include interaction**
-lm_2 <- lm(earn ~ height + male + height*male, data=earnings)
-display(lm_2)
-coef2 <- coef(lm_2)
+#+ results='hide'
+fit_3 <- stan_glm(earnk ~ height + male + height*male, data=earnings)
+#+
+print(fit_3)
+#' for plotting scale back to dollar scale
+coef3 <- coef(fit_3)*1000
 
+#' **Plot linear model with interaction**
 #+ eval=FALSE, include=FALSE
-# figure for the book, don't display in the report
 pdf(root("Earnings/figs","heights3.pdf"), height=8.5, width=11)
+#+
 par(mar=c(6,6,5,2)+.1)
-plot(range(earnings$height), range(predict(lm_2)), type="n", xlab="height", ylab="predicted earnings", cex=.8, cex.lab=3, pch=20, cex.axis=3, mgp=c(4,1.5,0), yaxt="n", col="gray10", cex.main=3, main="Fitted regression with interactions,\nseparate lines for men and women", bty="l")
+plot(range(earnings$height), range(predict(fit_3)*1000), type="n", xlab="height", ylab="predicted earnings", cex=.8, cex.lab=3, pch=20, cex.axis=3, mgp=c(4,1.5,0), yaxt="n", col="gray10", cex.main=3, main="Fitted regression with interactions,\nseparate lines for men and women", bty="l")
 axis(2, c(20000,30000), cex.axis=3)
-abline(coef2[1], coef2[2], col="red", lwd=2)
-text(62, coef2[1] + coef2[2]*80, "women:\ny = -7 000 + 180x", cex=3, adj=0, col="red")
-abline(coef2[1]+coef2[3], coef2[2]+coef2[4], col="blue", lwd=2)
-text(68, coef2[1]+coef2[3] + (coef2[2]+coef2[4])*66, "men:\ny = -22 000 + 740x", cex=3, adj=0, col="blue")
+abline(coef3[1], coef3[2], col="red", lwd=2)
+text(62, coef3[1] + coef3[2]*80, "women:\ny = -7 000 + 180x", cex=3, adj=0, col="red")
+abline(coef3[1]+coef3[3], coef3[2]+coef3[4], col="blue", lwd=2)
+text(68, coef3[1]+coef3[3] + (coef3[2]+coef3[4])*66, "men:\ny = -22 000 + 740x", cex=3, adj=0, col="blue")
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-#' include interaction, ggplot version
+#' **Include interaction, ggplot version**
 ggplot(earnings, aes(height, earn)) +
   geom_blank() +
   geom_abline(
-    intercept = c(coef2[1], coef2[1] + coef2[3]),
-    slope = c(coef2[2], coef2[2] + coef2[4]),
+    intercept = c(coef3[1], coef3[1] + coef3[3]),
+    slope = c(coef3[2], coef3[2] + coef3[4]),
     color = c("red", "blue")
   ) +
   coord_cartesian(
-    ylim = range(predict(lm_2)),
+    ylim = range(predict(fit_3)*1000),
     xlim = range(earnings$height)
   ) +
   annotate(
     "text",
     x = c(62, 68),
-    y = c(coef2[1] + coef2[2] * 80, coef2[1]+coef2[3] + (coef2[2]+coef2[4])*66),
+    y = c(coef3[1] + coef3[2] * 80, coef3[1]+coef3[3] + (coef3[2]+coef3[4])*66),
     label = c("women:\ny = -7 000 + 180x", "men:\ny = -22 000 + 740x"),
     color = c("red", "blue"),
     size = 5, hjust = 0
@@ -165,37 +181,42 @@ ggplot(earnings, aes(height, earn)) +
     title = "Fitted regression with interactions,\nseparate lines for men and women"
   )
 
-#' ### Classical regression on log scale
+#' ### Linear regression on log scale
 
 #' **Models on log scale**
+#+ results='hide'
 earnings$log_earn <- log(earnings$earn)
-logmodel_1 <- lm(log_earn ~ height, data=earnings)
-display(logmodel_1)
+logmodel_1 <- stan_glm(log_earn ~ height, data=earnings)
 earnings$log10_earn <- log10(earnings$earn)
-display(lm(log10_earn ~ height, data=earnings))
-logmodel_2 <- lm(log_earn ~ height + male, data=earnings)
-display(logmodel_2)
+log10model_1 <- stan_glm(log10_earn ~ height, data=earnings)
+logmodel_2 <- stan_glm(log_earn ~ height + male, data=earnings)
 earnings$log_height <- log(earnings$height)
-loglogmodel_2 <- lm(log_earn ~ log_height + male, data=earnings)
-display(loglogmodel_2)
-logmodel_3 <- lm(log_earn ~ height + male + height:male, data=earnings)
-display(logmodel_3)
+loglogmodel_2 <- stan_glm(log_earn ~ log_height + male, data=earnings)
+logmodel_3 <- stan_glm(log_earn ~ height + male + height:male, data=earnings)
+#+
+print(logmodel_1, digits=2)
+print(log10model_1, digits=3)
+print(logmodel_2, digits=2)
+print(loglogmodel_2, digits=2)
+print(logmodel_3, digits=2)
 
 #' **Model on log scale with standardized interaction**
 earnings$z_height <- with(earnings, (height - mean(height))/sd(height))
-logmodel_3a <- lm(log_earn ~ z_height + male + z_height:male, data=earnings)
-display(logmodel_3a)
+#+ results='hide'
+logmodel_3a <- stan_glm(log_earn ~ z_height + male + z_height:male, data=earnings)
+#+
+print(logmodel_3a, digits=2)
 
 #' ### Bayesian regression with rstanarm
 
-#' **Bayesian model on log scale**
-M_1 <- stan_glm(log_earn ~ height + male, data = earnings)
-sims <- as.matrix(M_1)
+#' **Posterior uncertainty for log model**
+sims <- as.matrix(logmodel_2)
 n_sims <- nrow(sims)
 
+# **Plot posterior draws of linear model on log scale**
 #+ eval=FALSE, include=FALSE
-# figure for the book, don't display in the report
 postscript(root("Earnings/figs","heights.log1a.ps"), horizontal=TRUE)
+#+
 par(mar=c(6,6,4,2)+.1)
 plot(earnings$height + runif(n,-.2,.2), earnings$log_earn, xlab="height", ylab="log (earnings)", cex=.8, cex.lab=3, pch=20, cex.axis=3, yaxt="n", mgp=c(4,1.5,0), col="gray10",
       cex.main=3, main="Log regression, plotted on log scale")
@@ -205,9 +226,10 @@ for (i in subset){
   curve(sims[i,1] + sims[i,2]*x, lwd=0.5, col="gray30", add=TRUE)
 }
 curve(coef(M_1)[1] + coef(M_1)[2]*x, add=TRUE)
+#+ eval=FALSE, include=FALSE
 dev.off()
 
-#' Bayesian model on log scale, ggplot version
+# **Plot posterior draws of linear model on log scale, ggplto version**
 subset <- sample(n_sims, 10)
 ggplot(earnings, aes(height, log_earn)) +
   geom_jitter(height = 0, width = 0.25) +
@@ -217,8 +239,8 @@ ggplot(earnings, aes(height, log_earn)) +
     color = "darkgray"
   ) +
   geom_abline(
-    intercept = coef(M_1)[1],
-    slope = coef(M_1)[2]
+    intercept = coef(logmodel_2)[1],
+    slope = coef(logmodel_2)[2]
   ) +
   labs(
     x = "height",
@@ -226,20 +248,20 @@ ggplot(earnings, aes(height, log_earn)) +
     title = "Log regression, plotted on log scale"
   )
 
-#' **Bayesian logistic regression on non-zero earnings**
-fit_1a <- stan_glm(positive ~ height + male,
+#' **Logistic regression on non-zero earnings**
+fit_2a <- stan_glm(positive ~ height + male,
                    family = binomial(link = "logit"),
                    data = earnings_all)
-sims_1a <- as.matrix(fit_1a)
+sims_2a <- as.matrix(fit_2a)
 
-#' **Bayesian model on log scale**
-fit_1b <- stan_glm(log_earn ~ height + male, data = earnings)
-sims_1b <- as.matrix(fit_1b)
-print(fit_1a, digits=2)
-print(fit_1b, digits=2)
+#' **Linear regression on log scale**
+fit_2b <- stan_glm(log_earn ~ height + male, data = earnings)
+sims_2b <- as.matrix(fit_2b)
+print(fit_2a, digits=2)
+print(fit_2b, digits=2)
 
 #' **Predictions for a new person**
 new <- data.frame(height = 68, male = 0, positive=1)
-pred_1a <- posterior_predict(fit_1a, newdata=new)
-pred_1b <- posterior_predict(fit_1b, newdata=new)
-pred <- ifelse(pred_1a == 1, exp(pred_1b), 0)
+pred_2a <- posterior_predict(fit_2a, newdata=new)
+pred_2b <- posterior_predict(fit_2b, newdata=new)
+pred <- ifelse(pred_2a == 1, exp(pred_2b), 0)
