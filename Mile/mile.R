3aa2f53b24e8dce189c17ef4fb085677cc131490
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-06-17 18:42:30 +0300

    switch from lm to stan_glm in Mile

--- Mile/mile.R
+++ Mile/mile.R
@@ -13,7 +13,6 @@
 #+ setup, message=FALSE, error=FALSE, warning=FALSE
 library("rprojroot")
 root<-has_dirname("RAOS-Examples")$make_fix_file()
-library("arm")
 library("rstanarm")
 options(mc.cores = parallel::detectCores())
 
@@ -21,14 +20,15 @@ options(mc.cores = parallel::detectCores())
 mile <- read.table(root("Mile/data","mile2.txt"), header=TRUE)
 year <- mile$yr + mile$month/12
 seconds <- mile$min*60 + mile$sec
+data <- data.frame(mile, year, seconds)
 
 #' **Linear model*
-fit <- lm(seconds ~ year)
-display(fit, 3)
+fit <- stan_glm(seconds ~ year, data = data)
+print(fit, digits=3)
 
 #' **Predictions for 1900 and 2000**
 print(1007 -.393*c(1900,2000))  # Approx
-print(coef(fit)[1] + coef(fit)[2]*c(1900,2000)) # Exact
+print(coef(fit)[1] + coef(fit)[2]*c(1900,2000), digits=4) # Exact
 
 #' **Example of increasing trend**
 #+ eval=FALSE, include=FALSE
@@ -36,12 +36,12 @@ pdf(root("Mile/figs","aplusbx1a.pdf"), height=3.5, width=5)
 #+
 a <- 0.15
 b <- 0.4
-par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
-plot (c(0,2.2), c(0,a+2.2*b), pch=20, cex=.5, main="y = a + bx",
+par(mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
+plot(c(0,2.2), c(0,a+2.2*b), pch=20, cex=.5, main="y = a + bx",
   bty="l", type="n", xlab="x", ylab="y", xaxt="n", yaxt="n", xaxs="i", yaxs="i")
-axis (1, c(0,1,2))
-axis (2, c(a,a+b,a+2*b), c("a","a+b","a+2b"))
-abline (a, b)
+axis(1, c(0,1,2))
+axis(2, c(a,a+b,a+2*b), c("a","a+b","a+2b"))
+abline(a, b)
 #+ eval=FALSE, include=FALSE
 dev.off()
 
@@ -51,12 +51,12 @@ pdf(root("Mile/figs","aplusbx1b.pdf"), height=3.5, width=5)
 #+
 a <- 0.95
 b <- -0.4
-par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
-plot (c(0,2.2), c(0,a+.2), pch=20, cex=.5, main="y = a + bx (with b < 0)",
+par(mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
+plot(c(0,2.2), c(0,a+.2), pch=20, cex=.5, main="y = a + bx (with b < 0)",
   bty="l", type="n", xlab="x", ylab="y", xaxt="n", yaxt="n", xaxs="i", yaxs="i")
-axis (1, c(0,1,2))
-axis (2, c(a,a+b,a+2*b), c("a","a+b","a+2b"))
-abline (a, b)
+axis(1, c(0,1,2))
+axis(2, c(a,a+b,a+2*b), c("a","a+b","a+2b"))
+abline(a, b)
 #+ eval=FALSE, include=FALSE
 dev.off()
 
@@ -64,8 +64,8 @@ dev.off()
 #+ eval=FALSE, include=FALSE
 pdf(root("Mile/figs","aplusbx2a.pdf"), height=3.5, width=5)
 #+
-par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
-curve (1007 - 0.393*x, from=0, to=2.1, xlab="x", ylab="y", bty="l",
+par(mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
+curve(1007 - 0.393*x, from=0, to=2.1, xlab="x", ylab="y", bty="l",
   main="y = 1007 - 0.393x")
 #+ eval=FALSE, include=FALSE
 dev.off()
@@ -74,8 +74,8 @@ dev.off()
 #+ eval=FALSE, include=FALSE
 pdf(root("Mile/figs","aplusbx2b.pdf"), height=3.5, width=5)
 #+
-par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
-curve (1007 - 0.393*x, from=0, to=100, xlab="x", ylab="y", bty="l",
+par(mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
+curve(1007 - 0.393*x, from=0, to=100, xlab="x", ylab="y", bty="l",
   main="y = 1007 - 0.393x")
 #+ eval=FALSE, include=FALSE
 dev.off()     
@@ -84,8 +84,9 @@ dev.off()
 #+ eval=FALSE, include=FALSE
 pdf(root("Mile/figs","aplusbx3.pdf"), height=3.5, width=5)
 #+
-par (mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
-curve (1007 - 0.393*x, from=1900, to=2000, xlab="Year", ylab="Time (seconds)", bty="l",
-  main="Approx. trend of record times in the mile run", ylim=c(210, 270))
+par(mar=c(3,3,1,1), mgp=c(2,.5,0), tck=-.01)
+plot(year, seconds)
+curve(1007 - 0.393*x, from=1900, to=2000, xlab="Year", ylab="Time (seconds)", bty="l",
+  main="Approx. trend of record times in the mile run", ylim=c(210, 270), add=TRUE)
 #+ eval=FALSE, include=FALSE
 dev.off()
