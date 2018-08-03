b0b8ddfb5fd7010f06045bef854862cedb5ddc87
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-08-04 00:34:51 +0300

    poststrat.R partially updated

--- Poststrat/poststrat.R
+++ Poststrat/poststrat.R
@@ -7,7 +7,8 @@
 #' Poststratification after estimation
 #' 
 #' The CBS News poll conducted from 12--16 October 2016 reported that, among likely voters who preferred one of the two major-party candidates, 45\% intended to vote for Donald Trump and 55\% for Hillary Clinton.  Of these respondents, Party ID 33\% Republican, 40\% Republican, 27\% independent.
-#' % source:  http://www.cbsnews.com/news/cbs-poll-clintons-lead-over-trump-widens-with-three-weeks-to-go/
+#' 
+#' source:  http://www.cbsnews.com/news/cbs-poll-clintons-lead-over-trump-widens-with-three-weeks-to-go/
 #' and https://www.scribd.com/document/327938789/CBS-News-Poll-10-17-toplines
 #'
 #' Effective sample size of likely voters
@@ -34,7 +35,6 @@
 #+ setup, message=FALSE, error=FALSE, warning=FALSE
 library("rstanarm")
 options(mc.cores = parallel::detectCores())
-library("arm")
 
 #' **Create some fake data**
 n_pid <- c(254, 282, 242)
@@ -51,82 +51,67 @@ for (j in 1:3){
   n_vote[[j]]<- c(n_vote[[j]], n_pid[j] - sum(n_vote[[j]]))
   vote <- c(vote, rep(c(1, 0, NA), n_vote[[j]]))
   y_bar_cells[j] <- mean(vote[pid==pid_names[j]], na.rm=TRUE)
-  pfround(y_bar_cells[j], 3)
+  round(y_bar_cells[j], 3)
 }
+poll <- data.frame(vote, pid)
 
 #' **Simple poststrat**
-pid_poststrat <- c(0.33, 0.36, 0.31)
-pfround(sum(pid_poststrat * y_bar_cells), 3)
+poststrat_data <- data.frame(pid=c("Republican", "Democrat", "Independent"),
+                             N=c(0.33, 0.36, 0.31))
+round(sum(poststrat_data$N * y_bar_cells), 3)
 
 #' ### Linear
 
-#' **lm**
-lm_1 <- lm(vote ~ factor(pid))
-display(lm_1, digits=2)
-
 #' **Raw estimate**
-pfround(mean(vote, na.rm=TRUE), digits=3)
-
-#' **Poststrat using predict()**
-poststrat_data <- data.frame(pid=c("Republican", "Democrat", "Independent"), N=c(0.33, 0.36, 0.31))
-predict_1a <- predict(lm_1, newdata=poststrat_data)
-est_1a <- sum(poststrat_data$N*predict_1a)/sum(poststrat_data$N)
-pfround(est_1a, digits=3)
+round(mean(poll$vote, na.rm=TRUE), 3)
 
 #' **stan_glm**
-fit_1 <- stan_glm(vote ~ factor(pid))
+fit_1 <- stan_glm(vote ~ factor(pid), data = poll)
 print(fit_1, digits=2)
 
-#' **Poststrat using posterior_predict()**
-predict_1b <- posterior_linpred(fit_1, newdata=poststrat_data)
-poststrat_est_1 <- predict_1b %*% poststrat_data$N/sum(poststrat_data$N)
-print(c(mean(poststrat_est_1), mad(poststrat_est_1)))
+#' **Poststrat using posterior_linpred()**
+predict_1 <- posterior_linpred(fit_1, newdata=poststrat_data)
+poststrat_est_1 <- predict_1 %*% poststrat_data$N/sum(poststrat_data$N)
+print(c(mean(poststrat_est_1), mad(poststrat_est_1)), digits=2)
 
 #' **Add extra uncertainty**
-n_sim <- nrow(predict_1b)
+n_sim <- nrow(predict_1)
 poststrat_est_2 <- poststrat_est_1 + rnorm(n_sim, 0, 0.02)
-print(c(mean(poststrat_est_2), mad(poststrat_est_2)))
+print(c(mean(poststrat_est_2), mad(poststrat_est_2)), digits=2)
 
 #' ### Logistic
 
 #' **Fit the regression**
-fit <- stan_glm(vote ~ factor(pid), family=binomial(link="logit"))
+fit <- stan_glm(vote ~ factor(pid), family=binomial(link="logit"), data = poll)
 print(fit, digits=2)
 
 #' **Raw estimate**
-pfround(mean(vote, na.rm=TRUE), digits=3)
-
-#' **Poststrat using the groups**
-predict_rep <- invlogit(coef(fit)[1] + coef(fit)[3])
-predict_dem <- invlogit(coef(fit)[1])
-predict_ind <- invlogit(coef(fit)[1] + coef(fit)[2])
-poststrat_est_1 <- predict_rep*pid_poststrat[1] + predict_dem*pid_poststrat[2] + predict_ind*pid_poststrat[3]
-pfround(poststrat_est_1, digits=3)
+round(mean(poll$vote, na.rm=TRUE), 3)
 
 #' **Poststrat using the predict function**
 X_population <- data.frame(pid=c("Republican", "Democrat", "Independent"))
 N_population <- c(0.33, 0.36, 0.31)
-predict_poststrat <- posterior_predict(fit, newdata=X_population)
+predict_poststrat <- colMeans(posterior_predict(fit, newdata=X_population))
 poststrat_est_2 <- sum(N_population*predict_poststrat)/sum(N_population)
-pfround(poststrat_est_2, digits=3)
+round(poststrat_est_2, digits=3)
 
 #' **Just to compare, poststrat using the original data**
 predict_a <- predict(fit, type="response")
-pfround(mean(predict_a), 3)
+round(mean(predict_a), 3)
 #' This doesn't work--it just spits back the raw estimate--because it's not using the external population info which is what makes poststrat work.
 #' 
 
-#' **More complicated example with two predictors**<br>
+#' **More complicated example with two predictors**
 #' 
 #' Imagine a model, `fit <- stan_glm(vote ~ factor(pid) + male, family=binomial(link="logit"))`
 #' The poststrat population matrix now has 6 rows and 2 columns:
 #' ```
-#' X_population <- data.frame(pid=rep(c("Republican", "Democrat", "Independent"), c(2,2,2)), male=rep(c(0,1), 2))
-#' N_population <- . . . [look it up]
-#' ```
-#' Continue...
+poll <- data.frame(pid=rep(c("Republican", "Democrat", "Independent"),
+                           c(2,2,2)), male=rep(c(0,1), 3))
+N_population <- c(0.16, 0.17, 0.19, 0.17, 0.16, 0.15)
 
-#' Quick test
+
+fit_2 <- stan_glm(vote ~ factor(pid) + male + factor(pid):male, data=poll)
 
 N <- 5
 x <- 1:N
