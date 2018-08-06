a5cbdbdaca390cdcbca2883d95090fce05d41625
Aki Vehtari <Aki.Vehtari@aalto.fi>
2018-08-06 11:00:11 +0300

    updated index

--- index.R
+++ index.R
@@ -9,38 +9,269 @@
 #' -------------
 #' 
 
-#' ### Code and data by chapters
+#' ## Code and data by chapters
 #' 
 
-#' ### Introduction
-#' - [ElectionsEconomy](ElectionsEconomy/)
-#' - [Helicopters](Helicopters/)
+#' ### 1  Introduction
+#'   - [ElectionsEconomy/](ElectionsEconomy/)
+#'     - [hibbs](ElectionsEconomy/hibbs.html) - Predicting presidential vote share from the economy
+#'   - [ElectricCompany/](ElectricCompany/)
+#'     - [electric](ElectricCompany/electric.html) - Analysis of "Electric company" data
+#'   - [Peacekeeping/](Peacekeeping/)
+#'     - [peace](Peacekeeping/peace.html) - Outcomes after civil war in countries with and without United Nations peacekeeping
+#'   - [SimpleCausal/](SimpleCausal/)
+#'     - [SimpleCausal](SimpleCausal/SimpleCausal.html) - Simple graphs illustrating regression for causal inference
+#'   - [Helicopters/](Helicopters/)
+#'     - [helicopters](Helicopters/helicopters.html) - Example data file for helicopter flying time exercise
 #' 
-#' ### Data and measurement
-#'  - healthdata.txt missing
+#' ### 2  Data and measurement
+#'   - [HDI/](HDI/)
+#'     - [hdi](HDI/hdi.html) - Human Development Index - Looking at data in different ways
+#'   - [Pew/](Pew/)
+#'     - [pew](Pew/pew.html) - Miscellaneous analyses using raw Pew data
+#'   - [HealthExpenditure/](HealthExpenditure/)
+#'     - [healthexpenditure](HealthExpenditure/healthexpenditure.html) - Discovery through graphs of data and models
+#'   - [Names/](Names/)
+#'     - [names](Names/names.html) - Names - Distributions of names of American babies
+#'   - [AgePeriodCohort/](AgePeriodCohort/)
+#'     - [births](AgePeriodCohort/births.html) - Age adjustment
 #' 
-#' ### Some basic methods in mathematics and probability
-#'  - [Mile](Mile/)
+#' ### 3  Some basic methods in mathematics and probability
+#'   - [Mile/](Mile/)
+#'     - [mile](Mile/mile.html) - Trend of record times in the mile run
+#'   - [Vitals/](Vitals/)
+#'     - [vitals](Vitals/vitals.html) - Predict weight
+#'   - [Stents/](Stents/)
+#'     - [stents](Stents/stents.html) - Stents - comparing distributions
 #' 
-#' ### Sampling distributions, generative models, and statistical inference
-#'   - [Girls](Girls/)
+#' ### 4  Generative models and statistical inference
+#'   - [Coverage/](Coverage/)
+#'     - [coverage](Coverage/coverage.html) - Example of coverage
+#'   - [Death/](Death/)
+#'     - [polls](Death/polls.html) - Proportion of American adults supporting the death penalty
+#'   - [Coop/](Coop/)
+#'     - [riverbay](Coop/riverbay.html) - Example of hypothesis testing
+#'   - [Girls/](Girls/)
 #' 
-#' ### Simulation
-#'   - [ProbabilitySimulation](ProbabilitySimulation/)
-#'   - [Earnings](Earnings/)
+#' ### 5  Simulation
+#'   - [ProbabilitySimulation/](ProbabilitySimulation/)
+#'     - [probsim](ProbabilitySimulation/probsim.html) - Simulation of probability models
+#'     - [earnings_bootstrap](Earnings/earnings_bootstrap.html) - Bootstrapping to simulate the sampling distribution
 #' 
-#' ### Background on regression modeling
-#'   - [Earnings](Earnings/)
-#'   - [PearsonLee](PearsonLee/)
-#'   - [FakeMidtermFinal](FakeMidtermFinal/)
+#' ### 6  Background on regression modeling
+#'   - [Simplest/](Simplest/)
+#'     - [simplest](Simplest/simplest.html) - Linear regression with a single predictor
+#'     - [simplest_lm](Simplest/simplest_lm.html) - Linear least squares regression with a single predictor
+#'   - [Earnings/](Earnings/)
+#'     - [earnings_regression](Earnings/earnings_regression.html) - Predict respondents' yearly earnings using survey data from 1990.
+#'   - [PearsonLee/](PearsonLee/)
+#'     - [PearsonLee](PearsonLee/PearsonLee.html) - The heredity of height. Published in 1903 by Karl Pearson and Alice Lee.
+#'   - [FakeMidtermFinal/](FakeMidtermFinal/)
+#'     - [FakeMidtermFinal](FakeMidtermFinal/FakeMidtermFinal.html) - Fake dataset of 1000 students' scores on a midterm and final exam
 #' 
-#' ### Linear regression with a single predictor
-#'   - [Simplest](Simplest/)
-#'   - [ElectionsEconomy](ElectionsEconomy/)
+#' ### 7  Linear regression with a single predictor
+#'   - [ElectionsEconomy/](ElectionsEconomy/)
+#'     - [hibbs](ElectionsEconomy/hibbs.html) - Predicting presidential vote share from the economy
+#'     - [hills](ElectionsEconomy/hills.html) - Present uncertainty in parameter estimates
+#'     - [hibbs_coverage](ElectionsEconomy/hibbs_coverage.html) - Checking the #'   - [Simplest/](Simplest/)
+#'     - [simplest](Simplest/simplest.html) - Linear regression with a single predictor
+#'     - [simplest_lm](Simplest/simplest_lm.html) - Linear least squares regression with a single predictor
 #' 
-#' ### Prediction and Bayesian inference
-#'   - [ElectionsEconomy](ElectionsEconomy/)
-#'   - [Vitals](Vitals/)
-#'   - [SexRatio](SexRatio/)
-#'   - [Earnings](Earnings/)
+#' ### 8  Bayesian inference and prediction
+#'   - [ElectionsEconomy/](ElectionsEconomy/)
+#'     - [hibbs](ElectionsEconomy/hibbs.html) - Predicting presidential vote share from the economy
+#'   - [SexRatio/](SexRatio/)
+#'     - [sexratio](SexRatio/sexratio.html) - Example where an informative prior makes a difference
+#'   - [Vitals/](Vitals/)
+#'     - [vitals](Vitals/vitals.html) - Predict weight
+#'   - [Earnings/](Earnings/)
+#'     - [earnings_regression](Earnings/earnings_regression.html) - Predict respondents' yearly earnings using survey data from 1990.
 #' 
+#' ### 9  Linear regression with multiple predictors
+#'   - [KidIQ/](KidIQ/)
+#'     - [kidiq](KidIQ/kidiq.html) - Linear regression with multiple predictors
+#'   - [Vitals/](Vitals/)
+#'     - [vitals](Vitals/vitals.html) - Predict weight
+#'   - [Congress/](Congress/)
+#'     - [congress](Congress/congress.html) - Predictive uncertainty for congressional elections
+#'   - [NES/](NES/)
+#'     - [nes_linear](NES/nes_linear.html) - Fitting the same regression to many datasets
+#'   - [Beauty/](Beauty/)
+#'     - [beauty](Beauty/beauty.html) - Student evaluations of instructors’ beauty and teaching quality
+#'
+#' ### 10  Diagnostics and model evaluation
+#'   - [KidIQ/](KidIQ/)
+#'     - [kidiq](KidIQ/kidiq.html) - Linear regression with multiple predictors
+#'     - [kidiq_loo](KidIQ/kidiq_loo.html) - Linear regression and leave-one-out cross-validation
+#'     - [kidiq_looR2](KidIQ/kidiq_looR2.html) - Linear regression and leave-one-out cross-validation R-squared
+#'     - [kidiq_kcv](KidIQ/kidiq_kcv.html) - Linear regression and K-fold cross-validation
+#'   - [Newcomb/](Newcomb/)
+#'     - [newcomb](Newcomb/newcomb.html) - Posterior predictive checking of Normal model for Newcomb's speed of light data
+#'   - [Unemployment/](Unemployment/)
+#'     - [unemployment](Unemployment/unemployment.html) - Time series fit and posterior predictive model checking for unemployment series
+#'   - [FakeKCV/](FakeKCV/)
+#'     - [fake_kcv](FakeKCV/fake_kcv.html) - Demonstration of $K$-fold cross-validation using simulated data
+#'   - [Pyth/](Pyth/)
+#' 
+#' ### 11  Transformations
+#'   - [KidIQ/](KidIQ/)
+#'     - [kidiq](KidIQ/kidiq.html) - Linear regression with multiple predictors
+#'   - [Earnings/](Earnings/)
+#'     - [earnings_regression](Earnings/earnings_regression.html) - Predict respondents' yearly earnings using survey data from 1990.
+#'   - [Mesquite/](Mesquite/)
+#'     - [mesquite](Mesquite/mesquite.html) - Predicting the yields of mesquite bushes
+#'   - [Pollution/](Pollution/)
+#' 
+#' ### 12  Logistic regression
+#'   - [NES/](NES/)
+#'     - [nes_logistic](NES/nes_logistic.html) - Logistic regression, identifiability, and separation
+#'   - [Arsenic/](Arsenic/)
+#'     - [arsenic_logistic_building](Arsenic/arsenic_logistic_building.html) - Building a logistic regression model: wells in Bangladesh
+#'     - [arsenic_logistic_residuals](Arsenic/arsenic_logistic_residuals.html) - Residual plots for a logistic regression model: wells in Bangladesh
+#'     - [arsenic_logistic_apc](Arsenic/arsenic_logistic_apc.html) - Average predictice comparisons for a logistic regression model: wells in Bangladesh
+#'   - [Rodents/](Rodents/)
+#'
+#' ### 13  Other generalized linear models
+#'   - [Roaches/](Roaches/)
+#'     - [roaches](Roaches/roaches.html) - Analyse the effect of integrated pes#'   - [Storable/](Storable/)
+#'     - stan_polr problem - Ordered categorical data analysis with a study fro#'   - [Earnings/](Earnings/)
+#'     - [earnings_compound](Earnings/earnings_compound.html) - Compound discrete-continuos model
+#'   - [RiskyBehavior/](RiskyBehavior/)
+#'   - [NES/](NES/)
+#'   - [Lalonde/](Lalonde/)
+#'   - [Congress/](Congress/)
+#'   - [AcademyAwards/](AcademyAwards/)
+#'
+#' ### 14  Design and sample size decisions
+#'   - [ElectricCompany/](ElectricCompany/)
+#'     - [electric](ElectricCompany/electric.html) - Analysis of "Electric company" data
+#'
+#' ### 15  Poststratification and missing-data imputation
+#'   - [Poststrat/](Poststrat/)
+#'     - [poststrat](Poststrat/poststrat.html) - Poststratification after estimation
+#'     - [poststrat2](Poststrat/poststrat2.html) - Poststratification after estimation
+#'   - [Imputation/](Imputation/)
+#'     - [imputation](Imputation/imputation.html) - Regression-based imputation for the Social Indicators Survey
+#' 
+#' 
+#' ## Code and data alphabetically
+#' 
+#'   - [AcademyAwards/](AcademyAwards/)
+#'   - [AgePeriodCohort/](AgePeriodCohort/)
+#'     - [births](AgePeriodCohort/births.html) - Age adjustment
+#'   - [Arsenic/](Arsenic/)
+#'     - [arsenic_logistic_building](Arsenic/arsenic_logistic_building.html) - Building a logistic regression model: wells in Bangladesh
+#'     - [arsenic_logistic_residuals](Arsenic/arsenic_logistic_residuals.html) - Residual plots for a logistic regression model: wells in Bangladesh
+#'     - [arsenic_logistic_apc](Arsenic/arsenic_logistic_apc.html) - Average predictice comparisons for a logistic regression model: wells in Bangladesh
+#'   - [Balance/](Balance/)
+#'     - [treatcontrol](Balance/treatcontrol.html)
+#'   - [Beauty/](Beauty/)
+#'     - [beauty](Beauty/beauty.html) - Student evaluations of instructors’ beauty and teaching quality
+#'   - [Bypass/](Bypass/)
+#'   - [CausalDiagram/](CausalDiagram/)
+#'     - [diagrams](CausalDiagram/diagrams.html) - Plot causal diagram
+#'   - [Coins/](Coins/)
+#'   - [Congress/](Congress/)
+#'     - [congress](Congress/congress.html) - Predictive uncertainty for congressional elections
+#'   - [Coop/](Coop/)
+#'     - [riverbay](Coop/riverbay.html) - Example of hypothesis testing
+#'   - [Coverage/](Coverage/)
+#'     - [coverage](Coverage/coverage.html) - Example of coverage
+#'   - [Cows/](Cows/)
+#'   - [DataCollection/](DataCollection/)
+#'     - [simulation](DataCollection/simulation.html) - Standard errors, parameterization and interactions
+#'   - [Death/](Death/)
+#'     - [polls](Death/polls.html) - Proportion of American adults supporting the death penalty
+#'   - [DifferentSoftware/](DifferentSoftware/)
+#'     - [linear](DifferentSoftware/linear.html) - Linear regression using different software options
+#'   - [Earnings/](Earnings/)
+#'     - [earnings_regression](Earnings/earnings_regression.html) - Predict respondents' yearly earnings using survey data from 1990.
+#'     - [earnings_bootstrap](Earnings/earnings_bootstrap.html) - Bootstrapping to simulate the sampling distribution
+#'     - [earnings_compound](Earnings/earnings_compound.html) - Compound discrete-continuos model
+#'   - [ElectionsEconomy/](ElectionsEconomy/)
+#'     - [hibbs](ElectionsEconomy/hibbs.html) - Predicting presidential vote share from the economy
+#'     - [hills](ElectionsEconomy/hills.html) - Present uncertainty in parameter estimates
+#'     - [hibbs_coverage](ElectionsEconomy/hibbs_coverage.html) - Checking the model-fitting procedure using fake-data simulation.
+#'   - [ElectricCompany/](ElectricCompany/)
+#'     - [electric](ElectricCompany/electric.html) - Analysis of "Electric company" data
+#'   - [FakeKCV/](FakeKCV/)
+#'     - [fake_kcv](FakeKCV/fake_kcv.html) - Demonstration of $K$-fold cross-validation using simulated data
+#'   - [FakeMidtermFinal/](FakeMidtermFinal/)
+#'     - [FakeMidtermFinal](FakeMidtermFinal/FakeMidtermFinal.html) - Fake dataset of 1000 students' scores on a midterm and final exam
+#'   - [FrenchElection/](FrenchElection/)
+#'     - [ps_primaire](FrenchElection/ps_primaire.html) - French Election data
+#'   - [Gay/](Gay/)
+#'     - [gay](Gay/gay.html) - Nonlinear models (Loess, B-spline, GP-spline, and BART) and political attitudes as a function of age
+#'   - [Girls/](Girls/)
+#'   - [Golf/](Golf/)
+#'     - [golf](Golf/golf.html) - Gold putting accuracy: Fitting a nonlinear model using Stan
+#'   - [HDI/](HDI/)
+#'     - [hdi](HDI/hdi.html) - Human Development Index - Looking at data in different ways
+#'   - [HealthExpenditure/](HealthExpenditure/)
+#'     - [healthexpenditure](HealthExpenditure/healthexpenditure.html) - Discovery through graphs of data and models
+#'   - [Helicopters/](Helicopters/)
+#'     - [helicopters](Helicopters/helicopters.html) - Example data file for helicopter flying time exercise
+#'   - [Imputation/](Imputation/)
+#'     - [imputation](Imputation/imputation.html) - Regression-based imputation for the Social Indicators Survey
+#'   - [Interactions/](Interactions/)
+#'     - [interactions](Interactions/interactions.html) - Plot interaction example figure
+#'   - [Introclass/](Introclass/)
+#'     - [fakeresid](Introclass/fakeresid.html) - Plot residuals vs.\ predicted values, or residuals vs.\ observed values?
+#'   - [KidIQ/](KidIQ/)
+#'     - [kidiq](KidIQ/kidiq.html) - Linear regression with multiple predictors
+#'     - [kidiq_loo](KidIQ/kidiq_loo.html) - Linear regression and leave-one-out cross-validation
+#'     - [kidiq_looR2](KidIQ/kidiq_looR2.html) - Linear regression and leave-one-out cross-validation R-squared
+#'     - [kidiq_kcv](KidIQ/kidiq_kcv.html) - Linear regression and K-fold cross-validation
+#'   - [Lalonde/](Lalonde/)
+#'   - [LogisticPriors/](LogisticPriors/)
+#'     - [logistic_priors](LogisticPriors/logistic_priors.html) - Effect of priors in logistic regression
+#'   - [LowBwt/](LowBwt/)
+#'   - [Mesquite/](Mesquite/)
+#'     - [mesquite](Mesquite/mesquite.html) - Predicting the yields of mesquite bushes
+#'   - [Mile/](Mile/)
+#'     - [mile](Mile/mile.html) - Trend of record times in the mile run
+#'   - [Names/](Names/)
+#'     - [names](Names/names.html) - Names - Distributions of names of American babies
+#'   - [NES/](NES/)
+#'     - [nes_linear](NES/nes_linear.html) - Fitting the same regression to many datasets
+#'     - [nes_logistic](NES/nes_logistic.html) - Logistic regression, identifiability, and separation
+#'   - [Newcomb/](Newcomb/)
+#'     - [newcomb](Newcomb/newcomb.html) - Posterior predictive checking of Normal model for Newcomb's speed of light data
+#'   - [Peacekeeping/](Peacekeeping/)
+#'     - [peace](Peacekeeping/peace.html) - Outcomes after civil war in countries with and without United Nations peacekeeping
+#'   - [PearsonLee/](PearsonLee/)
+#'     - [PearsonLee](PearsonLee/PearsonLee.html) - The heredity of height. Published in 1903 by Karl Pearson and Alice Lee.
+#'   - [Pew/](Pew/)
+#'     - [pew](Pew/pew.html) - Miscellaneous analyses using raw Pew data
+#'   - [Pollution/](Pollution/)
+#'   - [Poststrat/](Poststrat/)
+#'     - [poststrat](Poststrat/poststrat.html) - Poststratification after estimation
+#'     - [poststrat2](Poststrat/poststrat2.html) - Poststratification after estimation
+#'   - [ProbabilitySimulation/](ProbabilitySimulation/)
+#'     - [probsim](ProbabilitySimulation/probsim.html) - Simulation of probability models
+#'   - [Pyth/](Pyth/)
+#'   - [Redistricting/](Redistricting/)
+#'   - [Restaurant/](Restaurant/)
+#'     - [restaurant](Restaurant/restaurant.html) - Demonstration of using Stan for optimization
+#'   - [RiskyBehavior/](RiskyBehavior/)
+#'   - [Roaches/](Roaches/)
+#'     - [roaches](Roaches/roaches.html) - Analyse the effect of integrated pest management on reducing cockroach levels in urban apartments
+#'   - [Rodents/](Rodents/)
+#'   - [Sesame/](Sesame/)
+#'   - [SexRatio/](SexRatio/)
+#'     - [sexratio](SexRatio/sexratio.html) - Example where an informative prior makes a difference
+#'   - [SimpleCausal/](SimpleCausal/)
+#'     - [SimpleCausal](SimpleCausal/SimpleCausal.html) - Simple graphs illustrating regression for causal inference
+#'   - [Simplest/](Simplest/)
+#'     - [simplest](Simplest/simplest.html) - Linear regression with a single predictor
+#'     - [simplest_lm](Simplest/simplest_lm.html) - Linear least squares regression with a single predictor
+#'   - [SIS/](SIS/)
+#'     - [impute](SIS/impute.html) - Regression-based imputation for the Social Indicators Survey
+#'   - [Stents/](Stents/)
+#'     - [stents](Stents/stents.html) - Stents - comparing distributions
+#'   - [Storable/](Storable/)
+#'     - stan_polr problem - Ordered categorical data analysis with a study from experimental economics, on the topic of ``storable votes.''
+#'   - [Unemployment/](Unemployment/)
+#'     - [unemployment](Unemployment/unemployment.html) - Time series fit and posterior predictive model checking for unemployment series
+#'   - [Vitals/](Vitals/)
+#'     - [vitals](Vitals/vitals.html) - Predict weight
