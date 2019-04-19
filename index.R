6faf5a2c992cdaaffd95faf7a657bb94893029ae
Aki Vehtari <Aki.Vehtari@aalto.fi>
2019-04-19 12:21:49 -0400

    added link to git repo

--- index.R
+++ index.R
@@ -8,7 +8,7 @@
 #'
 #' - [Code and data by chapters](#by_chapters)
 #' - [Code and data folders alphabetically](#alphabetically)
-#' 
+#' - [Git repo for the code and data](https://github.com/avehtari/RAOS-Examples)
 #' -------------
 #' 
 
