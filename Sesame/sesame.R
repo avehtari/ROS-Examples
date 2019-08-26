96a63d7c67b47fee79e2dd4f1df1cf07b9de4810
Aki Vehtari <Aki.Vehtari@aalto.fi>
2019-08-26 18:10:30 +0300

    add Sesame

--- Sesame/sesame.R
+++ Sesame/sesame.R
@@ -1,10 +1,10 @@
 #' ---
-#' title: "Regression and Other Stories: Electric Company"
+#' title: "Regression and Other Stories: Sesame street"
 #' author: "Andrew Gelman, Jennifer Hill, Aki Vehtari"
 #' date: "`r format(Sys.Date())`"
 #' ---
 
-#' Analysis of "Electric company" data
+#' Causal analysis of Sesame Street experiment
 #' 
 #' -------------
 #' 
@@ -17,12 +17,13 @@ library("rprojroot")
 root<-has_dirname("RAOS-Examples")$make_fix_file()
 library("rstanarm")
 options(mc.cores = parallel::detectCores())
+library("brms")
 library("foreign")
 
 #' **Load data**
 sesame <- read.dta(file=root("Sesame/data","sesame.dta"))
-sesame$encouraged <- sesame$encour
-sesame$watched <- sesame$regular
+sesame$watched <- ifelse(sesame$viewcat==1, 0, 1)
+sesame$encouraged <- ifelse(sesame$viewenc==2, 0, 1)
 sesame$y <- sesame$postlet
 sesame$pretest <- sesame$prelet
 
@@ -31,41 +32,60 @@ sesame$pretest <- sesame$prelet
 fit_1a <- stan_glm(watched ~ encouraged, data=sesame, refresh=0)
 print(fit_1a, digits=2)
 
-#' **Estimate the percentage of children actually induced to watch
-#' Sesame Street by the intervention**
-fit_1a <- stan_glm(watched ~ encouraged, data=sesame, refresh=0)
-print(fit_1a, digits=2)
-
-#' **Compute the intent-to-treat estimate**
-fit_1b <- stan_glm(y ~ encouraged, data=sesame, refresh=0)
+#' **Compute the intent-to-treat estimate, obtained in this case using
+#' the regression of the outcome (in the data, labeled {\tt postlet},
+#' that is, the {\em post}-treatment measurement of the {\em letter}
+#' recognition task) on the instrument**
+fit_1b <- stan_glm(postlet ~ encouraged, data=sesame, refresh=0)
 print(fit_1b, digits=2)
 
-#' **`Inflate'' by dividing by the percentage of children affected by the intervention**
+#' **``inflate'' by dividing by the percentage of children affected by
+#' the intervention**
 iv_est <- coef(fit_1b)["encouraged"] / coef(fit_1a)["encouraged"]
-print(iv_est, digits=2)
 
-#' **Regress the ``treatment'' variable on the randomized instrument**
+#' **Two stage approach**
+#'
+#' The first step is to regress the ``treatment'' variable---an
+#' indicator for regular watching (watched)---on the randomized
+#' instrument, encouragement to watch (encouraged).  Then we plug
+#' predicted values of watched into the equation predicting the letter
+#' recognition outcome.
 fit_2a <- stan_glm(watched ~ encouraged, data=sesame, refresh=0)
-#* **Plug predicted values into the equation predicting the outcome**
 sesame$watched_hat_2 <- fit_2a$fitted
-fit_2b <- stan_glm(y ~ watched_hat_2, data=sesame, refresh=0)
-print(fit_2b, digits=1)
+fit_2b <- stan_glm(postlet ~ watched_hat_2, data=sesame, refresh=0)
+print(fit_2b, digits = 2)
 
-#' **Same with adjusting for covariates**
-fit_3a <- stan_glm(watched ~ encouraged + pretest + as.factor(site) + setting,
-  data=sesame, refresh=0)
+#' **Two stage approach with adjusting for covariates in an instrumental variables framework**
+fit_3a <- stan_glm(watched ~ encouraged + prelet + as.factor(site) + setting,
+                   data=sesame, refresh=0)
 watched_hat_3 <- fit_3a$fitted
-fit_3b <- stan_glm(y ~ watched_hat_3 + pretest + as.factor(site) + setting,
-  data=sesame, refresh=0)
-print(fit_3b, digits=1)
+fit_3b <- stan_glm(postlet ~ watched_hat_3 + prelet + as.factor(site) + setting,
+                   data=sesame, refresh=0)
+print(fit_3b, digits = 2)
 
-#' **Same with saving the predictor matrix**
-fit_3c <- stan_glm(y ~ watched_hat_3 + pretest + as.factor(site) + setting,
-                   x = TRUE, data=sesame, refresh=0)
-
-#' **Compute the standard deviation of the adjusted residuals**
+#' **Regress the outcome on predicted compliance and covariates**
+#'
+#' Save the predictor matrix from this second-stage
+#' regression.
+fit_3c <- stan_glm(postlet ~ watched_hat_3 + prelet + as.factor(site) + setting,
+                   x=TRUE, data=sesame)
 X_adj <- fit_3c$x
 X_adj[,"watched_hat_3"] <- sesame$watched
-residual_sd_adj <- sd(sesame$y - X_adj %*% coef(fit_3c))
-se_adj <- se(fit_3c)["watched_hat_3"]*residual_sd_adj / sigma(fit_3c)
-print(se_adj, digits=2)
+#' **Compute the standard deviation of the adjusted residuals**
+residual_sd_adj <- sd(sesame$postlet - X_adj %*% coef(fit_3c))
+se_adj <- se(fit_3c)["watched_hat_3"] * residual_sd_adj / sigma(fit_3c)
+print(se_adj)
+
+#' **Performing two-stage approach automatically using brms**
+f1 <- bf(watched ~ encour)
+f2 <- bf(postlet ~ watched)
+IV_brm <- brm(f1 + f2, data=sesame)
+print(IV_brm)
+
+#' **Performing two-stage approach automatically using brms**
+#'
+#' Incorporate other pre-treatment variables as controls
+f1 <- bf(watched ~ encour + prelet + setting + factor(site))
+f2 <- bf(postlet ~ watched + prelet + setting + factor(site))
+IV_brm_2 <- brm(f1 + f2, data=sesame)
+print(IV_brm_2)
