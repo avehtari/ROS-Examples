3f3292e345ec222a99e477eb9f2869f2b64bb6e8
Aki Vehtari <Aki.Vehtari@aalto.fi>
2019-11-21 10:41:26 +0200

    updated Vitals/heightweight.R

--- Vitals/heightweight.R
+++ Vitals/heightweight.R
@@ -38,45 +38,62 @@ barplot (weight.counts.women, xlab="weight", ylab="Count", main="weights of wome
 
 #' **Normal distribution for heights of women**
 #+ eval=FALSE, include=FALSE
-if (savefigs) pdf(root("AgePeriodCohort/heights1a_women.pdf"), height=2, width=2.5)
+if (savefigs) pdf(root("Vitals/figs","heights1a_women.pdf"), height=2.5, width=3.5)
 #+
-par(mar=c(3,1,3,1), mgp=c(0,0,0), tck=-.01)
-curve(dnorm(x,63.7,2.7), 52, 81, xlab="height (inches)", ylab="", bty="n", yaxs="i", main="heights of women\n(normal distribution)", yaxt="n",mgp=c(2,.5,0),cex.main=.8)
+par(mar=c(3,1,3,1), tck=-.02)
+curve(dnorm(x,63.7,2.7), 52, 81, xlab="height (inches)", ylab="", bty="n", yaxs="i", main="heights of women\n(normal distribution)", yaxt="n",mgp=c(1.5,.5,0),cex.main=.9)
 #+ eval=FALSE, include=FALSE
 if (savefigs) dev.off()
 
 #' **Normal distribution for heights of men**
 #+ eval=FALSE, include=FALSE
-if (savefigs) pdf(root("AgePeriodCohort/heights1a_men.pdf"), height=2, width=2.5)
+if (savefigs) pdf(root("Vitals/figs","heights1a_men.pdf"), height=2.5, width=3.5)
 #+
-par(mar=c(3,1,3,1), mgp=c(0,0,0), tck=-.01)
-curve(dnorm(x,69.1,2.9), 52, 81, xlab="height (inches)", ylab="", bty="n", yaxs="i", main="heights of men\n(normal distribution)", yaxt="n",mgp=c(2,.5,0),cex.main=.8)
+par(mar=c(3,1,3,1), tck=-.02)
+curve(dnorm(x,69.1,2.9), 52, 81, xlab="height (inches)", ylab="", bty="n", yaxs="i", main="heights of men\n(normal distribution)", yaxt="n", mgp=c(1.5,.5,0), cex.main=.9)
 #+ eval=FALSE, include=FALSE
 if (savefigs) dev.off()
 
 #' **Mixture of normals distribution for heights of all adults**
 #+ eval=FALSE, include=FALSE
-if (savefigs) pdf(root("AgePeriodCohort/heights1b.pdf"), height=2, width=2.5)
+if (savefigs) pdf(root("Vitals/figs","heights1b.pdf"), height=2.5, width=3.5)
 #+
-par(mar=c(3,1,3,1), mgp=c(0,0,0), tck=-.01)
-curve(.52*dnorm(x,63.7,2.7)+.48*dnorm(x,69.1,2.9), 52, 81, xlab="height (inches)", ylab="", bty="n", yaxs="i", main="heights of all adults\n(not a normal distribution)", yaxt="n",mgp=c(2,.5,0),cex.main=.8)
+par(mar=c(3,1,3,1), tck=-.02)
+curve(.52*dnorm(x,63.7,2.7)+.48*dnorm(x,69.1,2.9), 52, 81, xlab="height (inches)", ylab="", bty="n", yaxs="i", main="heights of all adults\n(not a normal distribution)", yaxt="n",mgp=c(1.5,.5,0), cex.main=.9)
 #+ eval=FALSE, include=FALSE
 if (savefigs) dev.off()
 
 #' **Normal distribution for log weights of men**
 #+ eval=FALSE, include=FALSE
-if (savefigs) pdf(root("AgePeriodCohort/weights1a.pdf"), height=2, width=2.5)
+if (savefigs) pdf(root("Vitals/figs","weights1a.pdf"), height=2.5, width=3.5)
 #+
-par(mar=c(3,1,3,1), mgp=c(0,0,0), tck=-.01)
-curve (dnorm(x,5.13,.17), 4, 6, xlab="logarithm of weight in pounds", ylab="", bty="n", yaxs="i", main="log weights of men\n(normal distribution)", yaxt="n",mgp=c(2,.5,0),cex.main=.8)
+par(mar=c(3,1,3,1), tck=-.02)
+curve(dnorm(x,5.13,.17), 4, 6, xlab="logarithm of weight in pounds", ylab="", bty="n", yaxs="i", main="log weights of men\n(normal distribution)", yaxt="n", mgp=c(1.5,.5,0), cex.main=.9)
 #+ eval=FALSE, include=FALSE
 if (savefigs) dev.off()
 
 #' **Log-normal distribution for weights of men**
 #+ eval=FALSE, include=FALSE
-if (savefigs) pdf(root("AgePeriodCohort/weights1b.pdf"), height=2, width=2.5)
+if (savefigs) pdf(root("Vitals/figs","weights1b.pdf"), height=2.5, width=3.5)
 #+
-par(mar=c(3,1,3,1), mgp=c(0,0,0), tck=-.01)
-curve (dlnorm(x,5.13,.17), 50,350,xlab="weight in pounds", ylab="", bty="n", yaxs="i", main="weights of men\n(lognormal distribution)", yaxt="n",mgp=c(2,.5,0),cex.main=.8)
+par(mar=c(3,1,3,1), tck=-.02)
+curve(dlnorm(x,5.13,.17), 50, 350,xlab="weight in pounds", ylab="", bty="n", yaxs="i", main="weights of men\n(lognormal distribution)", yaxt="n", mgp=c(1.5,.5,0), cex.main=.9)
+#+ eval=FALSE, include=FALSE
+if (savefigs) dev.off()
+
+#+ eval=FALSE, include=FALSE
+if (savefigs) pdf(root("Vitals/figs","normal_picture.pdf"), height=3.5, width=7)
+#+
+par(mar=c(2,0,2,0), tck=-.01)
+curve(dnorm(x), -4, 4, xlab="", ylab="", bty="n", yaxs="i", main="normal distribution", xaxt="n", yaxt="n")
+axis(1, c(-4, -3, -2, -1,  0,  1, 2, 3, 4), c("", "-3", "-2", "-1",  "0",  "1", "2", "3", ""), mgp=c(1.5, .5, 0), cex.axis=1.2)
+colors <- c("gray70", "gray50", "gray30")
+for (i in 3:1){
+  grid <- seq(-i, i, .01)
+  polygon(c(grid, i, -i), c(dnorm(grid), 0, 0), col=colors[i])
+}
+text(0, .35*dnorm(0), "68%", cex=1.3)
+text(-1.5, .3*dnorm(1.5), "13.5%", cex=1.3)
+text(1.5, .3*dnorm(1.5), "13.5%", cex=1.3)
 #+ eval=FALSE, include=FALSE
 if (savefigs) dev.off()
