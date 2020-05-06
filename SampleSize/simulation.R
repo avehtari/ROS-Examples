437548942dcdcdb7f64fedc68ab1f352f17ae215
Aki Vehtari <Aki.Vehtari@aalto.fi>
2020-05-06 16:22:46 +0300

    minor edit in SampleSize

--- SampleSize/simulation.R
+++ SampleSize/simulation.R
@@ -26,10 +26,8 @@ x2 <- sample(c(-0.5,0.5), N, replace=TRUE)
 fake <- data.frame(c(y,x1,x2))
 
 #' **Fit models**
-#+ results='hide'
 fit_1a <- stan_glm(y ~ x1, data = fake, refresh = 0)
 fit_1b <- stan_glm(y ~ x1 + x2 + x1:x2, data = fake, refresh = 0)
-#+
 print(fit_1a)
 print(fit_1b)
 
@@ -38,10 +36,8 @@ x1 <- sample(c(0,1), N, replace=TRUE)
 x2 <- sample(c(0,1), N, replace=TRUE)
 
 #' **Fit models**
-#+ results='hide'
 fit_2a <- stan_glm(y ~ x1, data = fake, refresh = 0)
 fit_2b <- stan_glm(y ~ x1 + x2 + x1:x2, data = fake, refresh = 0)
-#+
 print(fit_2a)
 print(fit_2b)
 
@@ -51,9 +47,7 @@ x1 <- sample(c(-1,1), N, replace=TRUE)
 x2 <- sample(c(-1,1), N, replace=TRUE)
 
 #' **Fit models**
-#+ results='hide'
 fit_3a <- stan_glm(y ~ x1, data = fake, refresh = 0)
 fit_3b <- stan_glm(y ~ x1 + x2 + x1:x2, data = fake, refresh = 0)
-#+
 print(fit_3a)
 print(fit_3b)
