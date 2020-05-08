f37c29631d22789b121ae89c19d8d14a1c2cd34a
Aki Vehtari <Aki.Vehtari@aalto.fi>
2020-05-08 22:04:40 +0300

    updated Beauty

--- Beauty/beauty.R
+++ Beauty/beauty.R
@@ -23,28 +23,24 @@ library("ggplot2")
 library("bayesplot")
 theme_set(bayesplot::theme_default(base_family = "sans"))
 
-#' The data came originally as an Excel file (ProfEvaltnsBeautyPublic.xls)
-#' which was saved to a .csv file (comma-separated values).<br/>
-#' Read the data into R, including the variable names (headers)
-data <- read.csv(root("Beauty/data","ProfEvaltnsBeautyPublic.csv"), header=TRUE)
-# Rename the two variables for convenience
-data$beauty <- data$btystdave
-data$eval <- data$courseevaluation
+#' **Load data**
+beauty <- read.csv(root("Beauty/data","beauty.csv"))
+head(beauty)
 
 #' ### Do more beautiful profs get higher evaluations?
 #'
 
 #' **Make a scatterplot of data**
 par(mar=c(3,3,1,1), mgp=c(1.7, .5, 0), tck=-.01)
-plot(data$beauty, data$eval)
+plot(beauty$beauty, beauty$eval)
 
 #' **Fit a linear regression**
-fit_1 <- stan_glm(eval ~ beauty, data=data, refresh=0)
+fit_1 <- stan_glm(eval ~ beauty, data=beauty, refresh=0)
 print(fit_1, digits=2)
 
 #' **Make a scatterplot with regression lines**
 # Labeling the axes
-plot(data$beauty, data$eval, xlab="Beauty", ylab="Average teaching evaluation")
+plot(beauty$beauty, beauty$eval, xlab="Beauty", ylab="Average teaching evaluation")
 # Display the regression line, added onto the scatterplot (add=TRUE)
 coefs <- coef(fit_1)
 curve(coefs[1] + coefs[2]*x, add=TRUE)
@@ -54,7 +50,7 @@ curve(coefs[1] + coefs[2]*x + sigma, lty=2, add=TRUE)
 curve(coefs[1] + coefs[2]*x - sigma, lty=2, add=TRUE)
 
 #' ggplot version
-ggplot(data, aes(beauty, eval)) +
+ggplot(data=beauty, aes(beauty, eval)) +
   geom_point(size = 2, alpha = 0.75) +
   geom_abline(
     slope = rep(coefs[2], 3),
@@ -71,7 +67,7 @@ ggplot(data, aes(beauty, eval)) +
 #' ### Do things differ for male and female profs?  
 
 #' Parallel regression lines
-fit_2 <- stan_glm(eval ~ beauty + female, data=data, refresh=0)
+fit_2 <- stan_glm(eval ~ beauty + female, data=beauty, refresh=0)
 print(fit_2, digits=2)
 coefs2 <- coef(fit_2)
 
@@ -79,37 +75,37 @@ coefs2 <- coef(fit_2)
 # Set up a 2x2 grid of plots
 par(mfrow=c(2,2))
 # Make separate plot for men, ...
-plot(data$beauty[data$female==0], data$eval[data$female==0], xlim=range(data$beauty), ylim=range(data$eval),
+plot(beauty$beauty[beauty$female==0], beauty$eval[beauty$female==0], xlim=range(beauty$beauty), ylim=range(beauty$eval),
      xlab="Beauty", ylab="Average teaching evaluation", main="Men")
 curve(coefs2[1] + coefs2[2]*x + coefs2[3]*0, add=TRUE)
 # ... women, ...
-plot(data$beauty[data$female==1], data$eval[data$female==1], xlim=range(data$beauty), ylim=range(data$eval),
+plot(beauty$beauty[beauty$female==1], beauty$eval[beauty$female==1], xlim=range(beauty$beauty), ylim=range(beauty$eval),
       xlab="Beauty", ylab="Average teaching evaluation", main="Women")
 curve(coefs2[1] + coefs2[2]*x + coefs2[3]*1, add=TRUE)
 # ... and both sexes on the same plot
 # First make the plot with type="n" (which displays axes but does not plot
 #   the points), then plot the points and lines separately for each sex
-plot(data$beauty, data$eval, xlab="Beauty", ylab="Average teaching evaluation",
+plot(beauty$beauty, beauty$eval, xlab="Beauty", ylab="Average teaching evaluation",
       main="Both sexes", type="n")
-points(data$beauty[data$female==0], data$eval[data$female==0], col="blue")
+points(beauty$beauty[beauty$female==0], beauty$eval[beauty$female==0], col="blue")
 curve(coefs2[1] + coefs2[2]*x + coefs2[3]*0, add=TRUE, col="blue")
-points(data$beauty[data$female==1], data$eval[data$female==1], col="red")
+points(beauty$beauty[beauty$female==1], beauty$eval[beauty$female==1], col="red")
 curve(coefs2[1] + coefs2[2]*x + coefs2[3]*1, add=TRUE, col="red")
 
 #' ggplot versions
 # Men 
 gg_male <-
-  ggplot(subset(data, female == 0), aes(beauty, eval)) +
+  ggplot(subset(beauty, female == 0), aes(beauty, eval)) +
   geom_point() +
   geom_abline(slope = coefs2[2], intercept = coefs2[1], color = "darkgray")
 # Women
 gg_female <-
-  ggplot(subset(data, female == 1), aes(beauty, eval)) +
+  ggplot(subset(beauty, female == 1), aes(beauty, eval)) +
   geom_point() +
   geom_abline(slope = coefs2[2], intercept = coefs2[1] + coefs2[3], color = "darkgray")
 # Both
 gg_both <-
-  ggplot(data, aes(beauty, eval)) +
+  ggplot(data=beauty, aes(beauty, eval)) +
   geom_point(aes(color = factor(female)), show.legend = FALSE) +
   scale_color_manual(values = c("red", "blue")) +
   geom_abline(
@@ -122,15 +118,15 @@ gg_both <-
 bayesplot_grid(
   gg_male, gg_female, gg_both,
   grid_args = list(ncol = 2),
-  xlim = range(data$beauty),
-  ylim = range(data$eval),
+  xlim = range(beauty$beauty),
+  ylim = range(beauty$eval),
   titles = c("Men", "Women", "Both sexes")
 )
 
 #' ### Do things differ for male and female profs?  
 
 #' **Non-parallel regression lines**
-fit_3 <- stan_glm(eval ~ beauty + female + beauty*female, data=data, refresh=0)
+fit_3 <- stan_glm(eval ~ beauty + female + beauty*female, data=beauty, refresh=0)
 print(fit_3, digits=2)
 coefs3 <- coef(fit_3)
 
@@ -140,14 +136,14 @@ par(mfrow=c(1,2))
 # Display the parallel regression lines in gray and the non-parallel lines
 # in heavy black
 # Make separate plot for men ...
-plot(data$beauty[data$female==0], data$eval[data$female==0], xlim=range(data$beauty), ylim=range(data$eval),
+plot(beauty$beauty[beauty$female==0], beauty$eval[beauty$female==0], xlim=range(beauty$beauty), ylim=range(beauty$eval),
       xlab="Beauty", ylab="Average teaching evaluation", main="Men")
 curve(coefs2[1] + coefs2[2]*x + coefs2[3]*0,
        lwd=.5, col="gray", add=TRUE)
 curve(coefs3[1] + coefs3[2]*x + coefs3[3]*0 + coefs3[4]*x*0,
        lwd=2, col="black", add=TRUE)
 # ... and women
-plot (data$beauty[data$female==1], data$eval[data$female==1], xlim=range(data$beauty), ylim=range(data$eval),
+plot (beauty$beauty[beauty$female==1], beauty$eval[beauty$female==1], xlim=range(beauty$beauty), ylim=range(beauty$eval),
       xlab="Beauty", ylab="Average teaching evaluation", main="Women")
 curve(coefs2[1] +coefs2[2]*x +coefs2[3]*1,
        lwd=.5, col="gray", add=TRUE)
@@ -162,44 +158,32 @@ gg_female2 <- gg_female + geom_abline(intercept = coefs3[1] + coefs3[3], slope =
 bayesplot_grid(
   gg_male2, gg_female2,
   grid_args = list(ncol = 2),
-  xlim = range(data$beauty),
-  ylim = range(data$eval),
+  xlim = range(beauty$beauty),
+  ylim = range(beauty$eval),
   titles = c("Men", "Women")
 )
 
 #' ### More models
 
 #' **Add age**
-fit_4 <- stan_glm(eval ~ beauty + female + age, data=data, refresh=0)
+fit_4 <- stan_glm(eval ~ beauty + female + age, data=beauty, refresh=0)
 print(fit_4, digits=2)
 
 #' **Add minority**
-fit_5 <- stan_glm(eval ~ beauty + female + minority, data=data, refresh=0)
+fit_5 <- stan_glm(eval ~ beauty + female + minority, data=beauty, refresh=0)
 print(fit_5, digits=2)
 
 #' **Add nonenglish**
-fit_6 <- stan_glm(eval ~ beauty + female + nonenglish, data=data, refresh=0)
+fit_6 <- stan_glm(eval ~ beauty + female + nonenglish, data=beauty, refresh=0)
 print(fit_6, digits=2)
 
 #' **Add nonenglish and lower**
 fit_7 <- stan_glm(eval ~ beauty + female + nonenglish + lower,
-                  data=data, refresh=0)
+                  data=beauty, refresh=0)
 print(fit_7, digits=2)
 
 #' ### Go back to simple model, add course indicators
 
-#' Create the course index variable
-courses <- data[, 18:47]   # (indicators for the 30 courses)
-n <- nrow(data)
-J <- ncol(courses) + 1
-course_id <- rep(0, n)
-for (i in 1:n){
-  for (j in 1:30){
-    if (courses[i,j]==1) course_id[i] <- j
-  }
-}
-data$course_id <- course_id
-
 #' **Include course indicators in a regression**
-fit_8 <- stan_glm(eval ~ beauty + factor(course_id), data=data, refresh=0)
+fit_8 <- stan_glm(eval ~ beauty + factor(course_id), data=beauty, refresh=0)
 print(fit_8, digits=2)
