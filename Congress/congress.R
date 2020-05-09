b207b64af5c04899b1cac2d6c70725770aefb9a4
Aki Vehtari <Aki.Vehtari@aalto.fi>
2020-05-09 22:17:15 +0300

    fixed Congress

--- Congress/congress.R
+++ Congress/congress.R
@@ -12,68 +12,26 @@
 
 #+ setup, include=FALSE
 knitr::opts_chunk$set(message=FALSE, error=FALSE, warning=FALSE, comment=NA)
+# switch this to TRUE to save figures in separate files
+savefigs <- FALSE
 
 #' **Load packages**
 library("rprojroot")
 root<-has_dirname("ROS-Examples")$make_fix_file()
 library("rstanarm")
 
-#' **Load and pre-process data**
-congress_list <- vector("list", 49)
-for (i in 1:49){
-  year <- 1896 + 2*(i-1)
-  file <- root("Congress/data",paste(year, ".asc", sep=""))
-  data_year <- matrix(scan(file), byrow=TRUE, ncol=5)
-  data_year <- cbind(rep(year, nrow(data_year)), data_year)
-  dvote <- data_year[,5]
-  rvote <- data_year[,6]
-  vote <- dvote/(dvote + rvote)
-  inc <- data_year[,4]
-  inc[inc == -9] <- 0
-  missing <- dvote== -9 | rvote == -9
-  vote[missing & inc== -1] <- 0
-  vote[missing & inc== 1] <- 1
-  vote[dvote == 0] <- 0
-  vote[rvote == 0]  <- 1
-  congress_list[[i]] <- data.frame(data_year, vote, inc)
-}
-
-i86 <- (1986-1896)/2 + 1
-cong84 <- congress_list[[i86-1]]
-cong86 <- congress_list[[i86]]
-cong88 <- congress_list[[i86+1]]
-cong90 <- congress_list[[i86+2]]
-
-# Data have some errors so we'll do a crude search-and-destroy of apparent inconsistencies
-inc_fix <- function(data1, data2){
-  inc2_clean <- data2$inc
-  inc2_clean[data2$inc == -1 & data1$vote > 0.5] <- 1
-  inc2_clean[data2$inc == 1 & data1$vote < 0.5] <- -1
-  inc2_clean
-}
-
-#' Impute uncontested elections
-uncontested_adj <- function(vote){
-  vote[vote < 0.1] <- 0.25
-  vote[vote > 0.9] <- 0.75
-  return(vote)
-}
-congress <- data.frame(inc86=inc_fix(cong84, cong86), inc88=inc_fix(cong86, cong88), inc90=inc_fix(cong88, cong90),
-                       v86=cong86$vote, v88=cong88$vote, v90=cong90$vote,
-                       v86_adj=uncontested_adj(cong86$vote), v88_adj=uncontested_adj(cong88$vote), v90_adj=uncontested_adj(cong90$vote))
-congress$inc88[263] <- -1   # result of special election in Lousisiana 4th district
-write.csv(congress, "congress.csv")
-
+congress <- read.csv(root("Congress/data","congress.csv"))
 inconsistent <- (congress$inc88==(-1) & congress$v86 > 0.5) |  (congress$inc88==1 & congress$v86 < 0.5)
+head(congress)
 
 #' **Regression predicting 1988 from 1986**
 data88 <- data.frame(vote=congress$v88_adj, past_vote=congress$v86_adj, inc=congress$inc88)
-fit88 <- stan_glm(vote ~ past_vote + inc, data=data88)
+fit88 <- stan_glm(vote ~ past_vote + inc, data=data88, refresh=0)
 print(fit88, digits=2)
 
 #' **Simulation for inferences and predictions of new data points**</br>
 #' **Predict from 1988 to 1990**
-data90 <- data.frame(past_vote=congress$v88_adj, incumbency=congress$inc90)
+data90 <- data.frame(past_vote=congress$v88_adj, inc=congress$inc90)
 #' **Simulate predictive simulations of the vector of new outcomes**
 pred90 <- posterior_predict(fit88, newdata=data90)
 
@@ -87,9 +45,9 @@ for (s in 1:n_sims) {
 }
 
 #' **Our posterior mean and sd of how many districts the Dems will win**
-print(c(mean(demspred), sqrt(var(demspred))),digits=2)
+print(c(mean(dems_pred), sqrt(var(dems_pred))),digits=2)
 #' **Histogram of how many districts the Dems will win**
-hist(demspred)
+hist(dems_pred)
 
 
 #' **Graphs**
