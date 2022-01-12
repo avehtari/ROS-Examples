9172f3972826903244e3c8061b1f4c3c182145e6
Aki Vehtari <Aki.Vehtari@aalto.fi>
2022-01-12 22:23:23 +0200

    add rpackage rosdata for easy access to data

---
+++ rpackage/data-raw/rosdata.R
@@ -0,0 +1,104 @@
+oscars <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/AcademyAwards/oscars.csv")
+usethis::use_data(oscars, overwrite = TRUE)
+
+births <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/AgePeriodCohort/data/births.txt", header=TRUE)
+usethis::use_data(births, overwrite = TRUE)
+
+wells <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Arsenic/data/wells.csv")
+usethis::use_data(wells, overwrite = TRUE)
+
+beauty <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Beauty/data/beauty.csv")
+usethis::use_data(beauty, overwrite = TRUE)
+
+bypass <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Bypass/bypass.csv")
+usethis::use_data(bypass, overwrite = TRUE)
+
+cc2 <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Childcare/data/cc2.csv")
+usethis::use_data(cc2, overwrite = TRUE)
+
+chile <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/ChileSchools/data/chile.csv")
+usethis::use_data(chile, overwrite = TRUE)
+
+heads <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Coins/heads.csv")
+usethis::use_data(heads, overwrite = TRUE)
+
+congress <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Congress/data/congress.csv")
+usethis::use_data(congress, overwrite = TRUE)
+
+riverbay <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Coop/data/Riverbay.csv", header=FALSE, sep=",")
+usethis::use_data(riverbay, overwrite = TRUE)
+
+earnings <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Earnings/data/earnings.csv")
+usethis::use_data(earnings, overwrite = TRUE)
+
+electric <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/ElectricCompany/data/electric.csv")
+usethis::use_data(electric, overwrite = TRUE)
+
+naes04 <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Gay/data/naes04.csv")
+usethis::use_data(naes04, overwrite = TRUE)
+
+SIS <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Imputation/data/SIS.csv")
+usethis::use_data(SIS, overwrite = TRUE)
+
+incentives <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Incentives/data/incentives.csv")
+usethis::use_data(incentives, overwrite = TRUE)
+
+kidiq <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/KidIQ/data/kidiq.csv")
+usethis::use_data(kidiq, overwrite = TRUE)
+
+mile <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Mile/data/mile.csv")
+usethis::use_data(mile, overwrite = TRUE)
+
+allnames <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Names/data/allnames_clean.csv")
+usethis::use_data(allnames, overwrite = TRUE)
+
+pollution <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Pollution/data/pollution.csv")
+usethis::use_data(pollution, overwrite = TRUE)
+
+poll <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Poststrat/data/poll.csv")
+usethis::use_data(poll, overwrite = TRUE)
+
+risky <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/RiskyBehavior/data/risky.csv")
+usethis::use_data(risky, overwrite = TRUE)
+
+roaches <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Roaches/data/roaches.csv")
+usethis::use_data(roaches, overwrite = TRUE)
+
+sesame <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Sesame/data/sesame.csv")
+usethis::use_data(sesame, overwrite = TRUE)
+
+student <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Student/data/student-merged.csv")
+usethis::use_data(student, overwrite = TRUE)
+
+hibbs <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/ElectionsEconomy/data/hibbs.dat", header=TRUE)
+usethis::use_data(hibbs, overwrite = TRUE)
+
+psprimaire <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/FrenchElection/data/ps_primaire.txt", header=TRUE)
+usethis::use_data(psprimaire, overwrite = TRUE)
+
+golf <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Golf/data/golf.txt", header=TRUE, skip=2)
+usethis::use_data(golf, overwrite = TRUE)
+
+health <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/HealthExpenditure/data/healthdata.txt", header=TRUE)
+usethis::use_data(health, overwrite = TRUE)
+
+helicopters <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Helicopters/data/helicopters.txt", header=TRUE)
+usethis::use_data(helicopters, overwrite = TRUE)
+
+mesquite <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Mesquite/data/mesquite.dat", header=TRUE)
+usethis::use_data(mesquite, overwrite = TRUE)
+
+nes <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/NES/data/nes.txt")
+usethis::use_data(nes, overwrite = TRUE)
+
+newcomb <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Newcomb/data/newcomb.txt", header = TRUE)
+usethis::use_data(newcomb, overwrite = TRUE)
+
+heights <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/PearsonLee/data/Heights.txt", header=TRUE)
+usethis::use_data(heights, overwrite = TRUE)
+
+pyth <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Pyth/pyth.txt", header = TRUE)
+usethis::use_data(pyth, overwrite = TRUE)
+
+unemp <- read.table("https://raw.githubusercontent.com/avehtari/ROS-Examples/master/Unemployment/data/unemp.txt", header=TRUE)
+usethis::use_data(unemp, overwrite = TRUE)
