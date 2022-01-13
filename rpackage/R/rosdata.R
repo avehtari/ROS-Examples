14340c33fd745e07e7d97664cdac8fdef15e2366
Aki Vehtari <Aki.Vehtari@aalto.fi>
2022-01-13 10:35:01 +0200

    add package doc with list of data sets

---
+++ rpackage/R/rosdata.R
@@ -0,0 +1,49 @@
+#' rosdata: Data for Regression and Other Stories
+#'
+#' Data package for easy use of datasets from Regression and Other
+#' Stories examples.
+#' 
+#' @section Data sets:
+#' 
+#' \itemize{
+#'   \item \code{\link{allnames}}
+#'   \item \code{\link{beauty}}
+#'   \item \code{\link{births}}
+#'   \item \code{\link{bypass}}
+#'   \item \code{\link{cc2}}
+#'   \item \code{\link{chile}}
+#'   \item \code{\link{congress}}
+#'   \item \code{\link{earnings}}
+#'   \item \code{\link{electric}}
+#'   \item \code{\link{golf}}
+#'   \item \code{\link{heads}}
+#'   \item \code{\link{health}}
+#'   \item \code{\link{heights}}
+#'   \item \code{\link{helicopters}}
+#'   \item \code{\link{hibbs}}
+#'   \item \code{\link{incentives}}
+#'   \item \code{\link{kidiq}}
+#'   \item \code{\link{mesquite}}
+#'   \item \code{\link{mile}}
+#'   \item \code{\link{naes04}}
+#'   \item \code{\link{nes}}
+#'   \item \code{\link{newcomb}}
+#'   \item \code{\link{oscars}}
+#'   \item \code{\link{poll}}
+#'   \item \code{\link{pollution}}
+#'   \item \code{\link{psprimaire}}
+#'   \item \code{\link{pyth}}
+#'   \item \code{\link{risky}}
+#'   \item \code{\link{riverbay}}
+#'   \item \code{\link{roaches}}
+#'   \item \code{\link{sesame}}
+#'   \item \code{\link{SIS}}
+#'   \item \code{\link{student}}
+#'   \item \code{\link{unemp}}
+#'   \item \code{\link{wells}}
+#' }
+#' 
+#' @docType package
+#' @name rosdata
+NULL
+#> NULL
